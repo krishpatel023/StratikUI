19f1c63b2e400fdbccc314f938d4302b79e4eb3d
krishpatel023 <krishpatel0234@gmail.com>
2024-03-27 14:27:56 +0530

    added(primitive): Input Text -variant-7

--- src/packages/primitives/input_text/Input_7.tsx
+++ src/packages/primitives/input_text/Input_7.tsx
@@ -8,6 +8,10 @@ function InputText({
   placeholder: string;
   props?: any;
 }) {
+  // Don't change the formating. ONLY CHANGE THE COLOR !!!
+  const darkBackground = "peer-focus:dark:bg-slate-950";
+  const lightBackground = "peer-focus:bg-[#f1f7ff]";
+
   return (
     <div>
       <div className="relative text-s_primary">
@@ -27,7 +31,7 @@ function InputText({
         />
 
         <label
-          className={`peer-focus:base absolute left-2  z-10  transform px-2 text-xs text-s_textSecondary transition-all duration-300 peer-placeholder-shown:translate-y-[0.90rem] peer-placeholder-shown:text-sm peer-focus:-translate-y-1 peer-focus:bg-[#020617] peer-focus:text-xs peer-disabled:bg-transparent`}
+          className={`peer-focus:base absolute left-2  z-10  transform px-2 text-xs text-s_textSecondary transition-all duration-300 peer-placeholder-shown:translate-y-[0.90rem] peer-placeholder-shown:text-sm peer-focus:-translate-y-1 ${darkBackground} ${lightBackground} peer-focus:text-xs peer-disabled:bg-transparent`}
         >
           {label}
         </label>
@@ -44,34 +48,26 @@ function Demo() {
   );
 }
 
-const ButtonCode: string = `function Demo() {
-  return (
-    <div>
-      <InputText placeholder="Something..." />
-    </div>
-  );
-}
-// --------------------------------------------------------------
-// THIS IS THE BUTTON LOGIC (IF WANT THE RAW COMPONENT COPY THIS)
-// --------------------------------------------------------------
-
-function InputText({
+const ButtonCode: string = `function InputText({
+  label,
   placeholder,
   props,
 }: {
+  label: string;
   placeholder: string;
   props?: any;
 }) {
+
+  // Don't change the formating. ONLY CHANGE THE COLOR !!!
+  const darkBackground = "peer-focus:dark:bg-slate-950";
+  const lightBackground = "peer-focus:bg-[#f1f7ff]";
+
   return (
     <div>
-      <span className="text-s_textPrimary font-medium text-sm">{label}</span>
       <div className="relative text-s_primary">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center px-2.5">
-          {icon ? icon : null}
-        </div>
         <input
           type="text"
-          className="my-1 w-full bg-transparent bg-opacity-30 text-s_textPrimary placeholder:text-s_textSecondary py-2 pl-10 rounded-md border-[1px] border-s_primary focus:border-s_accent focus:outline-none focus:ring-2 shadow-sm"
+          className="peer placeholder:text-transparent focus:placeholder:text-s_textSecondary my-1 w-full bg-transparent bg-opacity-30 text-s_textPrimary  py-2 pr-10 pl-4 rounded-md border-[1px] border-s_primary focus:border-s_accent focus:outline-none focus:ring-2 shadow-sm"
           //ADD FOR DISABLED
           // className="disabled:cursor-not-allowed"
 
@@ -83,7 +79,21 @@ function InputText({
           placeholder={placeholder}
           {...props}
         />
+
+        <label
+          className={\`peer-focus:base absolute left-2  z-10  transform px-2 text-xs text-s_textSecondary transition-all duration-300 peer-placeholder-shown:translate-y-[0.90rem] peer-placeholder-shown:text-sm peer-focus:-translate-y-1 \${darkBackground} \${lightBackground} peer-focus:text-xs peer-disabled:bg-transparent\`}
+        >
+          {label}
+        </label>
       </div>
+    </div>
+  );
+}`;
+
+const DemoStr: string = `function Demo() {
+  return (
+    <div className="w-80">
+      <InputText placeholder="you@example.com" label="Email" />
     </div>
   );
 }`;
@@ -99,6 +109,11 @@ const Implementation: ImplementationNode[] = [
     title: "Code",
     content: ButtonCode,
   },
+  {
+    type: "code",
+    title: "Usage",
+    content: DemoStr,
+  },
 ];
 
 const Data: DataDescription = {
@@ -106,7 +121,7 @@ const Data: DataDescription = {
   description: "This is a text input with Floating Label",
   implementation: Implementation,
   component: Demo(),
-  version_included: "0.0.1",
-  display: false,
+  version_included: "0.0.2",
+  display: true,
 };
 export default Data;
