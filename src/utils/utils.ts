df7fd10a350b8fded431b8dce56a340efed75679
krishpatel023 <krishpatel0234@gmail.com>
2024-05-05 22:43:06 +0530

    version: updated to -v 0.1.0

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 // This is the latest public version. Please update this when a new version is released. There are version checks and 'new' tags based on this.
-export const CURRENT_VERSION = "0.0.1";
+export const CURRENT_VERSION = "0.1.0";
 
 // Default Mode of the page false -> light, true -> dark
 export const DEFAULT_MODE = true;
