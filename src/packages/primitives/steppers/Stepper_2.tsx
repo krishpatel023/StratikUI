7597adc1582803d7068164bcf03e9eb8eab9cd70
krishpatel023 <krishpatel0234@gmail.com>
2024-05-21 21:50:36 +0530

    primitive/add : stepper -v-2

---
+++ src/packages/primitives/steppers/Stepper_2.tsx
@@ -0,0 +1,55 @@
+import { DataDescription, ImplementationNode } from "@/utils/constants";
+import { ImplementStepper } from "./Stepper_2_Helper";
+
+function Demo() {
+  return (
+    <div className="w-full min-h-40 flex justify-center items-center">
+      <ImplementStepper />
+    </div>
+  );
+}
+
+const Code: string = `
+// Add your code snippet here
+`;
+
+const DemoString: string = `
+// Add your demo code snippet here
+`;
+
+const Implementation: ImplementationNode[] = [
+  {
+    type: "technology_used",
+    content: ["tailwind-css", "twMerge"],
+  },
+  {
+    type: "code",
+    content: [
+      {
+        name: "Name",
+        content: [
+          { language: "tsx", code: Code },
+          { language: "jsx", code: Code },
+        ],
+      },
+      {
+        name: "Implementation",
+        content: [
+          { language: "tsx", code: DemoString },
+          { language: "jsx", code: DemoString },
+        ],
+      },
+    ],
+  },
+];
+
+const Data: DataDescription = {
+  name: "Default Stepper",
+  description: "Component Description",
+  implementation: Implementation,
+  component: <Demo />,
+  version_included: "0.1.1",
+  display: true,
+};
+
+export default Data;
