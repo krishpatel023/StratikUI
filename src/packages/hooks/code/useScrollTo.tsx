655bfbb270d9b8e083a2cc14bf5b1d58b7d652dd
krishpatel023 <krishpatel0234@gmail.com>
2024-05-28 10:59:44 +0530

    hooks/add: useScrollTo

---
+++ src/packages/hooks/code/useScrollTo.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useEffect, useState, RefObject } from "react";
+
+const useScrollToTop = (
+  elementByRef?: RefObject<HTMLElement | null>,
+  threshold = 300,
+  elementById?: string
+) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const element = elementById
+    ? document.getElementById(elementById)
+    : elementByRef?.current;
+
+  const toggleVisibility = () => {
+    if (window.scrollY > threshold) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return [isVisible, scrollToTop] as const;
+};
+
+export default useScrollToTop;
