c19c65edab259666cd757ccf4add7c9411cbf0d2
krishpatel023 <krishpatel0234@gmail.com>
2024-05-28 18:03:58 +0530

    fix: display button (#34)
    The button was broken as it could not read the click event and didn't have the disable option.

--- src/packages/ui/Button.tsx
+++ src/packages/ui/Button.tsx
@@ -2,18 +2,31 @@ import { MouseEventHandler } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
-  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  onClick?: () => void; // Change this line
   children: React.ReactNode;
   className?: string;
+  disabled?: boolean;
 }
-export function Button({ onClick, children, className }: ButtonProps) {
+
+export function Button({
+  onClick,
+  children,
+  className,
+  disabled,
+}: ButtonProps) {
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    if (onClick) onClick(); // Call onClick only if it's defined
+  };
+
   return (
     <button
       className={twMerge(
-        "rounded border border-neutral-400 dark:border-neutral-700 bg-neutral-200 dark:bg-neutral-900 py-2 px-4 text-s_textPrimary h-max",
+        "rounded border border-neutral-400 dark:border-neutral-700 bg-neutral-200 dark:bg-neutral-900 py-2 px-4 text-s_textPrimary h-max hover:cursor-pointer",
+        disabled && "cursor-not-allowed opacity-50",
         className
       )}
-      onClick={onClick}
+      onClick={handleClick}
+      disabled={disabled}
     >
       {children}
     </button>
