871e5a906bd16ee5fdcbbc319c26a7d452540764
krishpatel023 <krishpatel0234@gmail.com>
2024-05-29 15:15:33 +0530

    add: env typesafety (@t3-oss/env-nextjs)

---
+++ src/env.js
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "test", "production"]),
+  },
+  client: {
+    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NODE_ENV: z.enum(["development", "test", "production"]),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
+});
