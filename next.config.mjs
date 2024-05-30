33f544935d6f2486aaa4dcb1a74d5823b02963e7
krishpatel023 <krishpatel0234@gmail.com>
2024-05-30 11:00:40 +0530

    add: setup (#38)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextMDX from "@next/mdx";
+import { remarkPlugins } from "./src/utils/mdx/remark.mjs";
+import { rehypePlugins } from "./src/utils/mdx/rehype.mjs";
+import { recmaPlugins } from "./src/utils/mdx/recma.mjs";
+import withSearch from "./src/utils/mdx/search.mjs";
 
-export default nextConfig;
+const withMDX = nextMDX({
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+  },
+});
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+export default withSearch(withMDX(nextConfig));
