f5d2a7abd9edd2f9c1017ff95712cf809eca3615
krishpatel023 <krishpatel0234@gmail.com>
2024-06-01 18:32:46 +0530

    modified: added searchbar state in useInternalState hook (#39)

--- src/hooks/useInternalState.tsx
+++ src/hooks/useInternalState.tsx
@@ -5,9 +5,13 @@ import { createContext, useContext, useState } from "react";
 const InternalStateContext = createContext<{
   sidebar: boolean;
   setSidebar: (open: boolean) => void;
+  searchbar: boolean;
+  setSearchbar: (open: boolean) => void;
 }>({
   sidebar: false,
   setSidebar: () => {},
+  searchbar: false,
+  setSearchbar: () => {},
 });
 
 export const useInternalState = () => useContext(InternalStateContext);
@@ -18,9 +22,12 @@ export const InternalStateProvider = ({
   children: React.ReactNode;
 }) => {
   const [sidebar, setSidebar] = useState<boolean>(false);
+  const [searchbar, setSearchbar] = useState<boolean>(false);
 
   return (
-    <InternalStateContext.Provider value={{ sidebar, setSidebar }}>
+    <InternalStateContext.Provider
+      value={{ sidebar, setSidebar, searchbar, setSearchbar }}
+    >
       {children}
     </InternalStateContext.Provider>
   );
