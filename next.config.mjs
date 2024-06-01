3d65d5156ad36bdcfc1a1294213f6c6d011fe9f7
krishpatel023 <krishpatel0234@gmail.com>
2024-06-01 18:15:54 +0530

    delete: removed all the mdx files (#38)

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-import nextMDX from "@next/mdx";
-import { remarkPlugins } from "./src/utils/mdx/remark.mjs";
-import { rehypePlugins } from "./src/utils/mdx/rehype.mjs";
-import { recmaPlugins } from "./src/utils/mdx/recma.mjs";
-import withSearch from "./src/utils/mdx/search.mjs";
-
-const withMDX = nextMDX({
-  options: {
-    remarkPlugins,
-    rehypePlugins,
-    recmaPlugins,
-  },
-});
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-};
-
-export default withSearch(withMDX(nextConfig));
+module.exports = nextConfig;
--- src/components/mdx/Break.tsx
+++
@@ -1,3 +0,0 @@
-export default function Break(height: number) {
-  return <div className="min-w-full" style={{ height: `${height}rem` }}></div>;
-}
