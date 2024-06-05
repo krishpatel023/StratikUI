f5cf55bae6586e73436440fe9d0254975396fc71
krishpatel023 <krishpatel0234@gmail.com>
2024-06-05 23:58:11 +0530

    component/add: cookie consent -v2

---
+++ src/packages/components/cookie_consent/Cookie_2.tsx
@@ -0,0 +1,56 @@
+import { DataDescription, ImplementationNode } from "@/utils/constants";
+import { CookiePrompt } from "./Cookie_2_Helper";
+
+function Demo() {
+  return (
+    <div className="w-full min-h-[40rem]">
+      <CookiePrompt />
+    </div>
+  );
+}
+
+const Code: string = `
+// Add your code snippet here
+`;
+
+const DemoString: string = `
+// Add your demo code snippet here
+`;
+
+const Implementation: ImplementationNode[] = [
+  {
+    type: "technology_used",
+    content: ["tailwind-css", "twMerge"],
+  },
+  {
+    type: "code",
+    content: [
+      {
+        name: "Name",
+        content: [
+          { language: "tsx", code: Code },
+          { language: "jsx", code: Code },
+        ],
+      },
+      {
+        name: "Implementation",
+        content: [
+          { language: "tsx", code: DemoString },
+          { language: "jsx", code: DemoString },
+        ],
+      },
+    ],
+  },
+];
+
+const Data: DataDescription = {
+  name: "Default Cookie Consent",
+  description:
+    "This is paired up with useCookies hook. It uses various functionalities related to user preferences and is just a good demo for that. Click Cookie Settings to see it in action.",
+  implementation: Implementation,
+  component: <Demo />,
+  version_included: "0.1.1",
+  display: true,
+};
+
+export default Data;
