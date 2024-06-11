a7768a72b93e21a0dac452b2090524be68247591
krishpatel023 <krishpatel0234@gmail.com>
2024-06-12 01:47:45 +0530

    add: combined redundant code (#46)

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,16 @@
-import { H1, H2, TEXT } from "@/components/mdx/Headings";
-import ImageComponent from "@/components/mdx/Image";
-import { Anchor, BLOCK_QUOTE, LI, OL, UL } from "@/components/mdx/Misc";
+import {
+  Anchor,
+  BLOCK_QUOTE,
+  LI,
+  OL,
+  UL,
+  ImageComponent,
+  H1,
+  H2,
+  TEXT,
+  H3,
+  BACKTICK,
+} from "@/components/mdx/MDX";
 import { TABLE, TD, TH, TR } from "@/components/mdx/Table";
 import type { MDXComponents } from "mdx/types";
 
@@ -8,7 +18,11 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => H1({ children }),
     h2: ({ children }) => H2({ children }),
-    p: (props) => TEXT(props),
+    h3: ({ children }) => H3({ children }),
+    h4: ({ children }) => TEXT({ children }),
+    h5: ({ children }) => TEXT({ children }),
+    h6: ({ children }) => TEXT({ children }),
+    p: ({ children }) => TEXT({ children }),
     img: (props) => <ImageComponent {...props} />,
     // LIST
     ul: (props) => <UL {...props} />,
@@ -22,6 +36,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     // MISC
     blockquote: (props) => <BLOCK_QUOTE {...props} />,
     a: (props) => Anchor(props),
+    code: (props) => <BACKTICK {...props} />,
     ...components,
   };
 }
