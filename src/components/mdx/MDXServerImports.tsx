f0375f1f983024ff3a839b070e0cde93d5bf28a6
krishpatel023 <krishpatel0234@gmail.com>
2024-06-12 01:52:23 +0530

    add: Combined imports for dynamic importing (#46)
    Combined ann the required imports for dynamic imports. Divided the files based on the client/server req for optimization in page load. Also improves readability

---
+++ src/components/mdx/MDXServerImports.tsx
@@ -0,0 +1,15 @@
+import { Implementation } from "@/components/mdx/Implementation";
+import { convertToDashed } from "@/utils/utils";
+import { DefaultDisplay as Display } from "./DefaultDisplay";
+import { HeaderDefault } from "./HeaderToggle";
+
+import { TechnologyUsed } from "./TechnologyUsed";
+import { Preview } from "./Preview";
+export {
+  Implementation,
+  Preview,
+  convertToDashed,
+  HeaderDefault,
+  Display,
+  TechnologyUsed,
+};
