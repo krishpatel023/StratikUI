591ea90efc7ae826bce7c0c7f0cd7c4de23980ec
krishpatel023 <krishpatel0234@gmail.com>
2024-06-11 16:11:39 +0530

    add: module type of mdx - mdx.d.ts (#46)

---
+++ mdx.d.ts
@@ -0,0 +1,14 @@
+// mdx.d.ts
+declare module "*.mdx" {
+  import { ComponentType } from "react";
+
+  export const Data: {
+    title: string;
+    tags: string[];
+    description: string;
+    version_included: string;
+    id: string;
+  };
+  const MDXComponent: ComponentType;
+  export default MDXComponent;
+}
