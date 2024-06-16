043b9e0a08266551aaf87a666cd109f7cd4311dd
krishpatel023 <krishpatel0234@gmail.com>
2024-06-16 20:35:11 +0530

    fix: minor bug fixes

--- src/packages/primitives/input_text/Input_2.tsx
+++ src/packages/primitives/input_text/Input_2.tsx
@@ -12,89 +12,117 @@ import {
 } from "react-aria-components";
 import { twJoin, twMerge } from "tailwind-merge";
 
-export function Field({ name, type, isRequired, ...props }: TextFieldProps) {
+export function Field({ className, ...props }: TextFieldProps) {
   return (
     <TextField
-      name={name}
-      type={type}
-      isRequired={isRequired}
-      className={twMerge("flex flex-col", props.className as string)}
+      className={twMerge("flex flex-col", className as string)}
       {...props}
     />
   );
 }
 
-export function InputLabel({ children, ...props }: LabelProps) {
+export function InputLabel({ className, ...props }: LabelProps) {
   return (
     <Label
       className={twMerge(
-        "text-black dark:text-neutral-300 text-sm font-medium",
-        props.className as string
+        "text-foreground text-sm font-medium",
+        className as string
       )}
     >
-      {children}
+      {props.children}
     </Label>
   );
 }
-export interface ItemPropsExtended extends InputProps {
-  state?: "default" | "error" | "success" | "disabled";
-}
 
-export function InputField({ state = "default", ...props }: ItemPropsExtended) {
+export function InputBox({ className, ...props }: InputProps) {
   return (
     <Input
-      data-state={state}
       className={twMerge(
         twJoin(
           "w-full py-2 px-4 bg-transparent border-2 rounded focus:outline-none focus:ring-2 mt-1",
           "text-foreground placeholder:text-secondary-foreground border-outline-secondary  hover:border-outline focus:border-accent",
-          // "dark:text-white dark:placeholder:text-neutral-300 dark:border-neutral-800 dark:hover:border-neutral-700 dark:focus:ring-blue-500 dark:focus:border-blue-900",
           "disabled:cursor-not-allowed disabled:opacity-50 disabled:border-muted-secondary disabled:hover:border-muted-secondary",
-          "data-[state=error]:border-error data-[state=error]:hover:border-error-secondary data-[state=error]:focus:ring-error-secondary data-[state=error]:focus:border-error",
-          "data-[state=success]:border-success data-[state=success]:hover:border-success-secondary data-[state=success]:focus:ring-success-secondary data-[state=success]:focus:border-success"
+          "invalid:border-error invalid:hover:border-error-secondary invalid:focus:ring-error-secondary invalid:focus:border-error"
         ),
-        props.className as string
+        className as string
       )}
-      disabled={state === "disabled"}
       {...props}
     />
+  );
+}
+
+export interface InputFieldProps extends InputProps {
+  state?: "default" | "isInvalid" | "isDisabled";
+  label?: string;
+  isRequired?: boolean;
+  isReadOnly?: boolean;
+}
+
+export function InputField({
+  state = "default",
+  label,
+  isRequired,
+  isReadOnly,
+  ...props
+}: InputFieldProps) {
+  return (
+    <Field
+      name={props.name}
+      type={props.type}
+      className="w-80"
+      isInvalid={state === "isInvalid"}
+      isDisabled={state === "isDisabled"}
+      isReadOnly={isReadOnly}
+      isRequired={isRequired}
+    >
+      <InputLabel>{label}</InputLabel>
+      <InputBox {...props} />
+      <FieldError />
+    </Field>
   );
 }
 
 export const StatefulInputComponent = () => {
   return (
-    <div className="w-full flex flex-col items-center">
-      <ArrowHeading text="Default" className="mb-2" />
+    <div className="w-full flex flex-col items-center gap-6">
+      <span>
+        <ArrowHeading text="Default" className="mb-2" />
+        <InputField name="Name" type="text" placeholder="Name" label="Name" />
+      </span>
 
-      <Field name="Name" type="text" className="w-80">
-        <InputLabel>Name</InputLabel>
-        <InputField placeholder="Name" />
-        <FieldError />
-      </Field>
+      <span>
+        <ArrowHeading text="Error" className="mb-2" />
+        <InputField
+          name="Name"
+          type="text"
+          label="Name"
+          placeholder="Name"
+          state="isInvalid"
+        />
+      </span>
 
-      <ArrowHeading text="Success" className="mb-2" />
+      <span>
+        <ArrowHeading text="Disabled" className="mb-2" />
+        <InputField
+          name="Name"
+          type="text"
+          label="Name"
+          placeholder="Name"
+          state="isDisabled"
+        />
+      </span>
 
-      <Field name="Name" type="text" className="w-80">
-        <InputLabel>Name</InputLabel>
-        <InputField placeholder="Name" state="success" />
-        <FieldError />
-      </Field>
-
-      <ArrowHeading text="Error" className="mb-2" />
-
-      <Field name="Name" type="text" className="w-80">
-        <InputLabel>Name</InputLabel>
-        <InputField placeholder="Name" state="error" />
-        <FieldError />
-      </Field>
-
-      <ArrowHeading text="Disabled" className="mb-2" />
-
-      <Field name="Name" type="text" className="w-80">
-        <InputLabel>Name</InputLabel>
-        <InputField placeholder="Name" state="disabled" />
-        <FieldError />
-      </Field>
+      <span>
+        <ArrowHeading text="ReadOnly" className="mb-2" />
+        <InputField
+          name="Name"
+          type="text"
+          label="Name"
+          placeholder="Name"
+          isReadOnly
+          value={"John Doe"}
+        />
+      </span>
     </div>
   );
 };
