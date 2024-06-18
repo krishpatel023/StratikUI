b8ced088c5552a95d0da63194fde8f2314dc3c06
krishpatel023 <krishpatel0234@gmail.com>
2024-06-18 19:14:51 +0530

    fix: useFileUpload (#46)

---
+++ src/packages/hooks/useFileUpload/Demo.tsx
@@ -0,0 +1,9 @@
+import { FileUpload_1 } from "@/packages/primitives/file_upload/File_Upload_1_Helper";
+
+export function Demo() {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <FileUpload_1 />
+    </div>
+  );
+}
