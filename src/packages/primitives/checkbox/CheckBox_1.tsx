890a8f67cb5a0f81df50a1b691ec9b9da8cf9435
krishpatel023 <krishpatel0234@gmail.com>
2024-06-26 15:20:49 +0530

    add: checkbox (#47)

--- src/packages/primitives/checkbox/CheckBox_1.tsx
+++ src/packages/primitives/checkbox/CheckBox_1.tsx
@@ -1,47 +1,41 @@
-import { DataDescription, ImplementationNode } from "@/utils/constants";
-import { CheckBox, Demo } from "./CheckBox_1_Helper";
+"use client";
 
-const Code: string = `
-// Add your code snippet here
-`;
+import {
+  CheckboxProps,
+  Checkbox as ReactAriaCheckbox,
+} from "react-aria-components";
+import { twMerge } from "tailwind-merge";
 
-const DemoString: string = `
-// Add your demo code snippet here
-`;
-
-const Implementation: ImplementationNode[] = [
-  {
-    type: "technology_used",
-    content: ["tailwind-css", "twMerge"],
-  },
-  {
-    type: "code",
-    content: [
-      {
-        name: "Name",
-        content: [
-          { language: "tsx", code: Code },
-          { language: "jsx", code: Code },
-        ],
-      },
-      {
-        name: "Implementation",
-        content: [
-          { language: "tsx", code: DemoString },
-          { language: "jsx", code: DemoString },
-        ],
-      },
-    ],
-  },
-];
-
-const Data: DataDescription = {
-  name: "Circular Checkbox",
-  description: "Component Description",
-  implementation: Implementation,
-  component: <Demo />,
-  version_included: "0.0.1",
-  display: true,
+export const Checkbox = ({ children, className, ...props }: CheckboxProps) => {
+  return (
+    <ReactAriaCheckbox
+      className={twMerge(
+        "group text-foreground hover:cursor-pointer flex gap-2 ",
+        className as string
+      )}
+      {...props}
+    >
+      <>
+        <div className="mt-[0.125rem] min-w-5 max-w-5 min-h-5 max-h-5 flex items-start justify-center border border-outline rounded group-data-[selected=true]:bg-accent group-data-[focused=true]:ring-2 group-data-[focused=true]:ring-accent transition-all duration-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            aria-hidden="true"
+            className="w-full h-full fill-none group-data-[selected=true]:fill-accent-foreground"
+          >
+            <path
+              fillRule="evenodd"
+              d="M13.488 3.43a.75.75 0 0 1 .081 1.058l-6 7a.75.75 0 0 1-1.1.042l-3.5-3.5A.75.75 0 0 1 4.03 6.97l2.928 2.927l5.473-6.385a.75.75 0 0 1 1.057-.081Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        {children}
+      </>
+    </ReactAriaCheckbox>
+  );
 };
 
-export default Data;
+export const CheckboxImplementation = () => {
+  return <Checkbox>Checkbox</Checkbox>;
+};
