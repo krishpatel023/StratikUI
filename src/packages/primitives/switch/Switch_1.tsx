7a6a3f0f2665c9a6a66bcb7f413deabc074b1396
krishpatel023 <krishpatel0234@gmail.com>
2024-06-26 15:20:19 +0530

    fix: minor bug fixes in switch (#47)

--- src/packages/primitives/switch/Switch_1.tsx
+++ src/packages/primitives/switch/Switch_1.tsx
@@ -7,23 +7,25 @@ export interface ExtendedSwitchProps extends SwitchProps {
   children?: React.ReactNode;
 }
 
-export const Switch = ({ className, ...props }: ExtendedSwitchProps) => {
+export const Switch = ({ className, ...props }: SwitchProps) => {
   return (
     <ReactAriaSwitch
       className={twMerge(
-        "flex gap-4 justify-center items-center group hover:cursor-pointer disabled:cursor-not-allowed",
+        "flex gap-4 justify-center items-center group hover:cursor-pointer disabled:cursor-not-allowed text-foreground",
         className as string
       )}
       {...props}
     >
-      <div className="bg-secondary rounded-full w-14 flex justify-start items-center border border-outline  group-data-[selected=true]:justify-end group-data-[selected=true]:bg-accent group-focus:ring-2 group-focus:ring-accent group-disabled:opacity-50">
-        <span className="w-5 h-5 m-1 rounded-full bg-accent group-data-[selected=true]:bg-accent-foreground transition-transform duration-300 ease-linear" />
-      </div>
-      {props.children}
+      <>
+        <div className="bg-secondary rounded-full min-w-14 max-w-14 flex justify-start items-center border border-outline  group-data-[selected=true]:justify-end group-data-[selected=true]:bg-accent group-focus:ring-2 group-focus:ring-accent group-disabled:opacity-50 transition-all duration-200 ease-linear">
+          <span className="min-w-5 max-w-5 min-h-5 max-h-5 m-1 rounded-full bg-accent group-data-[selected=true]:bg-accent-foreground transition-all duration-200 ease-linear" />
+        </div>
+        {props.children}
+      </>
     </ReactAriaSwitch>
   );
 };
 
 export const SwitchImplementation = () => {
-  return <Switch> Label Message </Switch>;
+  return <Switch>Label Message</Switch>;
 };
