e0ebe6f74835dff9129e0c6b6d751b0f7c06abb7
krishpatel023 <krishpatel0234@gmail.com>
2024-07-02 19:10:28 +0530

    add: dropdown (#47)

--- src/packages/primitives/dropdown/Dropdown_1.tsx
+++ src/packages/primitives/dropdown/Dropdown_1.tsx
@@ -1,62 +1,179 @@
-import {
-  DataDescription,
-  IconProps,
-  ImplementationNode,
-} from "@/utils/constants";
-import {
-  Dropdown,
-  ImplementDropdown,
-} from "@/packages/primitives/dropdown/Dropdown_1_Helper";
+"use client";
 
-function Demo() {
+import { Icons } from "@/utils/icons";
+import {
+  Button,
+  FieldError,
+  Header,
+  Label,
+  Menu,
+  MenuItem,
+  MenuItemProps,
+  MenuProps,
+  MenuTrigger as MenuTriggerAria,
+  MenuTriggerProps,
+  Popover,
+  Section,
+  Separator,
+  SubmenuTrigger as SubmenuTriggerAria,
+  SubmenuTriggerProps,
+  Text,
+} from "react-aria-components";
+import { twMerge } from "tailwind-merge";
+
+export interface DropdownTriggerProps extends MenuTriggerProps {
+  label?: string;
+  description?: string;
+  errorMessage?: string;
+}
+
+export function DropdownTrigger({ children, ...props }: DropdownTriggerProps) {
   return (
-    <div className="w-full h-[30rem] flex flex-col justify-evenly items-center">
-      <ImplementDropdown />
-    </div>
+    <MenuTriggerAria aria-label={props.label || "Dropdown"} {...props}>
+      <div className="flex flex-col">
+        {props.label && (
+          <Label className="text-primary-foreground text-sm">
+            {props.label}
+          </Label>
+        )}
+        {children}
+        {props.description && (
+          <Text slot="description" className="text-primary-foreground text-sm">
+            {props.description}
+          </Text>
+        )}
+        <FieldError className="text-sm text-error">
+          {props.errorMessage}
+        </FieldError>
+      </div>
+    </MenuTriggerAria>
   );
 }
 
-const Code: string = `
-// Add your code snippet here
-`;
+export function Dropdown({
+  items,
+  children,
+  className,
+  ...props
+}: MenuProps<any>) {
+  return (
+    <>
+      <Popover>
+        <Menu
+          items={items}
+          className={twMerge(
+            "w-full bg-primary border border-outline-secondary py-1 rounded outline-none focus-visible:outline-none",
+            className as string
+          )}
+          {...props}
+        >
+          {/* <Section> */}
+          <>{children}</>
+          {/* </Section> */}
+        </Menu>
+      </Popover>
+    </>
+  );
+}
 
-const DemoString: string = `
-// Add your demo code snippet here
-`;
+export function DropdownItem({ className, ...props }: MenuItemProps) {
+  return (
+    <MenuItem
+      aria-label="Dropdown Item"
+      className={twMerge(
+        "p-1 mx-1 rounded hover:bg-secondary hover:outline-none focus:bg-secondary focus:outline-none text-primary-foreground disabled:opacity-50 transition-colors duration-100 ease-linear hover:cursor-pointer",
+        className as string
+      )}
+      {...props}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
 
-const Implementation: ImplementationNode[] = [
-  {
-    type: "technology_used",
-    content: ["tailwind-css", "twMerge"],
-  },
-  {
-    type: "code",
-    content: [
-      {
-        name: "Name",
-        content: [
-          { language: "tsx", code: Code },
-          { language: "jsx", code: Code },
-        ],
-      },
-      {
-        name: "Implementation",
-        content: [
-          { language: "tsx", code: DemoString },
-          { language: "jsx", code: DemoString },
-        ],
-      },
-    ],
-  },
-];
+export function DropdownHeader({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <Header
+      className={twMerge("text-primary-foreground p-1 mx-1 mb-2", className)}
+    >
+      {children}
+    </Header>
+  );
+}
 
-const Data: DataDescription = {
-  name: "Component Name",
-  description: "Component Description",
-  implementation: Implementation,
-  component: <Demo />,
-  version_included: "0.1.1",
-  display: true,
+export function DropdownDivider({ className }: { className?: string }) {
+  return (
+    <Separator
+      className={twMerge(
+        "min-w-full min-h-[2px] border-b border-outline my-2",
+        className
+      )}
+    />
+  );
+}
+
+export const SubmenuTrigger = (props: SubmenuTriggerProps) => {
+  return (
+    <SubmenuTriggerAria {...props} aria-label={"Submenu"}>
+      {props.children}
+    </SubmenuTriggerAria>
+  );
 };
 
-export default Data;
+export function DropdownImplementation() {
+  return (
+    <DropdownTrigger
+      label="User Settings"
+      description="This is an user settings dropdown"
+    >
+      <Button className="w-full text-start px-2 py-1 rounded border border-outline-secondary bg-primary open:bg-secondary text-primary-foreground my-1 max-w-80">
+        Settings
+      </Button>
+      <Dropdown className="min-w-60">
+        <Section>
+          <DropdownHeader>My Account</DropdownHeader>
+          <DropdownItem>Profile</DropdownItem>
+          <DropdownItem isDisabled>Billing</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+        </Section>
+        <DropdownDivider />
+        <Section>
+          <DropdownHeader className="mt-2">Other Settings</DropdownHeader>
+
+          <SubmenuTrigger>
+            <DropdownItem className="flex items-center justify-between">
+              <span>Team</span>
+              <Icons.chevron className="h-5 w-5" />
+            </DropdownItem>
+
+            <Dropdown className="min-w-40">
+              <Section>
+                <DropdownHeader>Team</DropdownHeader>
+              </Section>
+              <DropdownDivider />
+              <Section>
+                <DropdownItem>Team 1</DropdownItem>
+                <DropdownItem>Team 2</DropdownItem>
+                <DropdownItem>Team 3</DropdownItem>
+              </Section>
+              <DropdownDivider />
+              <DropdownItem>Add Team</DropdownItem>
+            </Dropdown>
+          </SubmenuTrigger>
+
+          <DropdownItem>Support</DropdownItem>
+        </Section>
+        <DropdownDivider />
+        <DropdownItem className="hover:bg-error focus:bg-error">
+          Logout
+        </DropdownItem>
+      </Dropdown>
+    </DropdownTrigger>
+  );
+}
