2e2e2dd79eea47aeea8f104433e1296cefb39e4e
krishpatel023 <krishpatel0234@gmail.com>
2024-07-09 15:14:23 +0530

    add: pagination_01 (#47)

--- src/packages/primitives/pagination/Pagination_1.tsx
+++ src/packages/primitives/pagination/Pagination_1.tsx
@@ -1,56 +1,66 @@
-import { DataDescription, ImplementationNode } from "@/utils/constants";
-import { CodeJsx, CodeTsx, Pagination } from "./Pagination_1_Helper";
+"use client";
 
-function Demo() {
+import { useState } from "react";
+import { Button, Group } from "react-aria-components";
+
+type PaginationProps = {
+  initialPage: number;
+  totalPage: number;
+  nextPageEvent?: () => void;
+  prevPageEvent?: () => void;
+};
+
+export function Pagination({
+  initialPage,
+  totalPage,
+  nextPageEvent,
+  prevPageEvent,
+}: PaginationProps) {
+  const [currentPage, setCurrentPage] = useState<number>(initialPage);
+
+  const handleNext = () => {
+    if (currentPage < totalPage) {
+      setCurrentPage(currentPage + 1);
+      if (nextPageEvent) nextPageEvent();
+    }
+  };
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      if (prevPageEvent) prevPageEvent();
+    }
+  };
+  return (
+    <Group className="flex justify-center gap-4 text-primary-foreground border-[1px] border-outline  rounded-md bg-primary focus-within:ring-2 focus-within:ring-secondary">
+      <Button
+        className="w-full py-2 px-4 border-r-[1px] border-outline disabled:cursor-not-allowed disabled:text-muted-foreground  pressed:bg-secondary  rounded-l-[inherit] outline-none"
+        onPress={handlePrev}
+        isDisabled={currentPage === 1}
+        slot="decrement"
+      >
+        Prev
+      </Button>
+      <div className="flex justify-center gap-2 py-2 px-4 max-w-20">
+        <span>{currentPage}</span>
+        <span>of</span>
+        <span>{totalPage}</span>
+      </div>
+      <Button
+        className="w-full py-2 px-4 border-l-[1px] border-outline disabled:cursor-not-allowed disabled:text-muted-foreground  pressed:bg-secondary  rounded-r-[inherit] outline-none"
+        onPress={handleNext}
+        isDisabled={currentPage === totalPage}
+        slot="increment"
+      >
+        Next
+      </Button>
+    </Group>
+  );
+}
+
+export function PaginationImplementation() {
   return (
     <div className="w-full flex justify-center">
       <Pagination initialPage={1} totalPage={10} />
     </div>
   );
 }
-
-const DemoString: string = `function Demo() {
-  return (
-    <div className="w-full flex justify-center">
-      <Pagination initialPage={1} totalPage={10} />
-    </div>
-  );
-}`;
-
-const Implementation: ImplementationNode[] = [
-  {
-    type: "technology_used",
-    content: ["tailwind-css"],
-  },
-  {
-    type: "code",
-    content: [
-      {
-        name: "Name",
-        content: [
-          { language: "tsx", code: CodeTsx },
-          { language: "jsx", code: CodeJsx },
-        ],
-      },
-      {
-        name: "Implementation",
-        content: [
-          { language: "tsx", code: DemoString },
-          { language: "jsx", code: DemoString },
-        ],
-      },
-    ],
-  },
-];
-
-const Data: DataDescription = {
-  name: "Default Pagination",
-  description:
-    "This is a simple pagination that is fully functional and ready to use.",
-  implementation: Implementation,
-  component: <Demo />,
-  version_included: "0.0.7",
-  display: true,
-};
-
-export default Data;
