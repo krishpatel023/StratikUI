7ab47654bb66cdaa35865e39d13bbd8619b08175
krishpatel023 <krishpatel0234@gmail.com>
2024-07-14 22:35:10 +0530

    add: hero_04 (#52)

--- src/packages/components/hero_section/Hero_4.tsx
+++ src/packages/components/hero_section/Hero_4.tsx
@@ -1,146 +1,123 @@
-import { DataDescription, ImplementationNode } from "@/utils/constants";
+"use client";
+
+import Logo from "@/packages/helper/Logo";
+import useMousePosition from "@/packages/primitives/containers/Container_3";
+import { useEffect, useRef, useState } from "react";
 
 import {
-  Carousel,
-  CarouselStringJsx,
-  CarouselStringTsx,
-} from "@/packages/components/hero_section/Hero_4_Helper";
-import { Background, BackgroundString } from "@/packages/helper/Background";
-import { LogoStringJsx, LogoStringTsx } from "@/packages/helper/Logo";
-export function Hero() {
+  Button as ReactAriaButton,
+  ButtonProps as ReactAriaButtonProps,
+} from "react-aria-components";
+import { twMerge } from "tailwind-merge";
+import { Background } from "@/packages/helper/Background";
+import { Button } from "@/packages/primitives/buttons/Button_1";
+
+export function BorderAnimationButton({
+  children,
+  className,
+}: ReactAriaButtonProps) {
+  const { x, y } = useMousePosition();
+  const [relativeX, setRelativeX] = useState(0);
+
+  const containerRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const rect = containerRef.current.getBoundingClientRect();
+
+      const dist = x - rect.left;
+      if (y > rect.top && y < rect.bottom && dist > 0 && dist < rect.width)
+        setRelativeX(dist);
+    }
+  }, [x]);
+
   return (
-    <div className="w-full min-h-screen overflow-hidden flex flex-col @md:flex-row gap-10 @md:gap-0 justify-center items-center  relative">
-      <div className="w-full my-24 flex flex-col justify-center items-center text-center gap-8 z-10">
-        <button className="text-s_textPrimary flex gap-2 border-2 border-s_accent rounded-full @lg:px-6 px-4 py-1 ">
+    <ReactAriaButton
+      className="group relative  rounded-full text-foreground overflow-hidden p-0.5"
+      ref={containerRef}
+    >
+      <div
+        className={twMerge(
+          "z-10 bg-background relative rounded-[inherit]",
+          className as string
+        )}
+      >
+        <>{children}</>
+      </div>
+      <div
+        className="z-0 m-1 mx-auto my-auto w-full h-full  absolute top-0 -left-1/2   bg-gradient-to-r from-transparent via-accent to-transparent opacity-40 group-hover:opacity-100 transition-opacity duration-500"
+        style={{ translate: relativeX ? `${relativeX}px` : "50%" }}
+      />
+    </ReactAriaButton>
+  );
+}
+
+export function Carousel({
+  direction = "left",
+}: {
+  direction?: "left" | "right";
+}) {
+  const animateInDirection =
+    direction === "right"
+      ? "animate-infinite-scroll-right"
+      : "animate-infinite-scroll-left";
+  return (
+    <>
+      <div className="max-w-[80rem] h-28 inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+        <ul
+          className={`flex items-center justify-center md:justify-start [&_li]:mx-8 @md:[&_li]:mx-16  ${animateInDirection}`}
+        >
+          {Logo.map((item, i) => (
+            <li key={i} className="p-4 hover:bg-secondary rounded-md">
+              <item.logo className="w-10 h-10 text-primary-foreground" />
+            </li>
+          ))}
+        </ul>
+        <ul
+          className={`flex items-center justify-center md:justify-start [&_li]:mx-8 @md:[&_li]:mx-16   ${animateInDirection}`}
+        >
+          {Logo.map((item, i) => (
+            <li key={i} className="p-4 hover:bg-secondary rounded-md">
+              <item.logo className="w-10 h-10 text-primary-foreground" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export function HeroSection() {
+  return (
+    <div className="w-full overflow-hidden flex flex-col @md:flex-row gap-10 @md:gap-0 justify-center items-center relative py-20">
+      <div className="w-full flex flex-col justify-center items-center text-center gap-8">
+        <BorderAnimationButton className="text-primary-foreground flex gap-2  rounded-full @lg:px-6 px-4 py-1 ">
           View all the latest components.
-          <span className="text-s_accent hidden @lg:block">Read More</span>
-        </button>
-        <h1 className="text-5xl font-bold  w-[90%] @lg:w-1/2 text-s_textPrimary">
-          Make the websites in lightspeed using Stratik UI
+          <span className="text-accent hidden @lg:block">Read More</span>
+        </BorderAnimationButton>
+        <h1 className="text-5xl font-bold  w-[90%] @lg:w-1/2 text-primary-foreground">
+          This is the best library for functional components.
         </h1>
-        <span className="text-2xl font-medium w-[90%] @lg:w-3/4 text-s_textSecondary">
+        <span className="text-2xl font-medium w-[90%] @lg:w-3/4 text-secondary-foreground">
           Beautifully designed components that you can copy and paste into your
           apps. Accessible. Customizable. Developer Friendly.
         </span>
         <div className="flex gap-8">
-          <button className="w-32 h-10 rounded-md bg-s_accent hover:bg-s_accentLight text-s_textComplementary">
+          <Button variant="accent" className="w-32">
             Get Started
-          </button>
-          <button className="text-s_textPrimary">Learn More</button>
+          </Button>
+          <Button variant="ghost" className="w-32 h-full">
+            Learn More
+          </Button>
         </div>
-        <h2 className="text-s_textPrimary font-semibold text-2xl mt-8">
-          Meet Our Partners
-        </h2>
-
-        <Carousel />
+        <div>
+          <h1 className="text-primary-foreground font-semibold text-2xl mt-8">
+            Meet Our Partners
+          </h1>
+          <Carousel />
+        </div>
       </div>
       <Background />
     </div>
   );
 }
-
-const Code: string = `function Hero() {
-  return (
-    <div className="w-full min-h-screen overflow-hidden flex flex-col @md:flex-row gap-10 @md:gap-0 justify-center items-center  relative">
-      <div className="w-full my-24 flex flex-col justify-center items-center text-center gap-8 z-10">
-        <button className="text-s_textPrimary flex gap-2 border-2 border-s_accent rounded-full @lg:px-6 px-4 py-1 ">
-          View all the latest components.
-          <span className="text-s_accent hidden @lg:block">Read More</span>
-        </button>
-        <h1 className="text-5xl font-bold  w-[90%] @lg:w-1/2 text-s_textPrimary">
-          Make the websites in lightspeed using Stratik UI
-        </h1>
-        <span className="text-2xl font-medium w-[90%] @lg:w-3/4 text-s_textSecondary">
-          Beautifully designed components that you can copy and paste into your
-          apps. Accessible. Customizable. Developer Friendly.
-        </span>
-        <div className="flex gap-8">
-          <button className="w-32 h-10 rounded-md bg-s_accent hover:bg-s_accentLight text-s_textComplementary">
-            Get Started
-          </button>
-          <button className="text-s_textPrimary">Learn More</button>
-        </div>
-        <h1 className="text-s_textPrimary font-semibold text-2xl mt-8">
-          Meet Our Partners
-        </h1>
-
-        <Carousel />
-      </div>
-      <Background />
-    </div>
-  );
-}`;
-
-const Implementation: ImplementationNode[] = [
-  {
-    type: "technology_used",
-    content: ["tailwind-css"],
-  },
-  {
-    type: "code",
-    content: [
-      {
-        name: "Hero",
-        content: [
-          {
-            code: Code,
-            language: "tsx",
-          },
-          {
-            code: Code,
-            language: "jsx",
-          },
-        ],
-      },
-      {
-        name: "Background",
-        content: [
-          {
-            code: BackgroundString,
-            language: "tsx",
-          },
-          {
-            code: BackgroundString,
-            language: "jsx",
-          },
-        ],
-      },
-      {
-        name: "Carousel",
-        content: [
-          {
-            code: CarouselStringTsx,
-            language: "tsx",
-          },
-          {
-            code: CarouselStringJsx,
-            language: "jsx",
-          },
-        ],
-      },
-      {
-        name: "Logo",
-        content: [
-          {
-            code: LogoStringTsx,
-            language: "tsx",
-          },
-          {
-            code: LogoStringJsx,
-            language: "jsx",
-          },
-        ],
-      },
-    ],
-  },
-];
-
-const HeroData: DataDescription = {
-  name: "Hero Section with Carousel",
-  description: "This is a hero section with carousel",
-  implementation: Implementation,
-  component: Hero(),
-  version_included: "0.0.2",
-  display: true,
-};
-export default HeroData;
