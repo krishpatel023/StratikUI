b77fd1c0932fca134a4cff6c2488d20ae17de3cd
krishpatel023 <krishpatel0234@gmail.com>
2024-07-20 12:32:10 +0530

    registry/add: theme (#53)

--- apps/registry/src/app/page.tsx
+++ apps/registry/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HOME
+    <main className="flex min-h-screen flex-col items-center text-center gap-4 p-24 text-foreground">
+      <h1 className="text-4xl font-bold">Welcome to the StratikUI Registry</h1>
+      <p className="text-lg italic">
+        This is a registry for all the components in the StratikUI library. The
+        development and storage of components is handled here.
+      </p>
     </main>
   );
 }
---
+++ apps/registry/src/utils/controllers.ts
@@ -0,0 +1,6 @@
+import packageJsonData from "../../package.json";
+// This is the latest public version. Please update this when a new version is released. There are version checks and 'new' tags based on this.
+export const CURRENT_VERSION = packageJsonData.version;
+
+// Default Mode of the page false -> light, true -> dark
+export const DEFAULT_THEME = true;
