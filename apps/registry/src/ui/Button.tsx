f54c04135e5e4fbd4a7cf867ecfa4d91c86ba0aa
krishpatel023 <krishpatel0234@gmail.com>
2024-07-20 12:53:19 +0530

    registry/rename: constants -> types (#53)

--- apps/registry/src/ui/Button.tsx
+++ apps/registry/src/ui/Button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { IconProps } from "@/utils/constants";
+import { IconProps } from "@/utils/types";
 import {
   Button as ReactAriaButton,
   ButtonProps as ReactAriaButtonProps,
