cd8e2e868159c292968db5c07d1b89c0d8a369b8
krishpatel023 <krishpatel0234@gmail.com>
2024-07-23 23:02:38 +0530

    registry/hooks/add: useClipboard (#56)

---
+++ apps/registry/src/packages/hooks/useClipboard/01/default-js/example.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import useClipboard from "@/packages/hooks/useClipboard/01/default-js/useClipboard";
+import Button from "@/ui/Button";
+import Message from "@/ui/Message";
+
+export default function UseClipboardExample() {
+  const { copy, read } = useClipboard();
+
+  const [value, setValue] = useState("Hello, World!");
+
+  const handleCopy = () => {
+    copy("Hello, World!");
+  };
+
+  const handleRead = async () => {
+    const text = await read();
+    if (text) setValue(text);
+  };
+
+  return (
+    <div className="text-foreground flex flex-col justify-center items-center gap-8 px-8">
+      <div className="flex gap-4">
+        <Button onPress={handleCopy}>Copy</Button>
+        <Button onPress={handleRead}>Read</Button>
+      </div>
+
+      <h1 className="text-2xl underline">Copied Value</h1>
+      <Message>
+        First Press Copy or copy anything you want and then press the read
+        button.
+      </Message>
+      <p>{value}</p>
+    </div>
+  );
+}
