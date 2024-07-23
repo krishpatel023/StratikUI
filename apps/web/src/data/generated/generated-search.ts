673cec7eb78bd49e4a9d31ff0a28f8bbf94aa5e4
krishpatel023 <krishpatel0234@gmail.com>
2024-07-23 19:03:44 +0530

    web/fix: build issue due to import path (#53)

--- apps/web/src/data/generated/generated-search.ts
+++ apps/web/src/data/generated/generated-search.ts
@@ -1,19 +1,15 @@
-import {SearchProps} from "@/data/Search"
+import { SearchProps } from "../Search";
 
 const Search: SearchProps = [
   {
-    "name": "useArtificialLoader",
-    "description": "A hook that simulates a loading state for a component.",
-    "tags": [
-      "hooks",
-      " artificial",
-      " loader"
-    ],
-    "link": "docs/hooks/useArtificialLoader#useartificialloader",
-    "version_included": "0.3.0",
-    "flag": "default",
-    "category": "hooks",
-    "group": "useArtificialLoader"
-  }
-]
-export default Search
+    name: "useArtificialLoader",
+    description: "A hook that simulates a loading state for a component.",
+    tags: ["hooks", " artificial", " loader"],
+    link: "docs/hooks/useArtificialLoader#useartificialloader",
+    version_included: "0.3.0",
+    flag: "default",
+    category: "hooks",
+    group: "useArtificialLoader",
+  },
+];
+export default Search;
