6e084d835079560e9c59585ac9689f1f41e1ee5d
krishpatel023 <krishpatel0234@gmail.com>
2024-07-24 01:48:53 +0530

    registry/hooks/add: useFullscreen (#56)

---
+++ apps/registry/src/packages/hooks/useFullscreen/01/default-js/example.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React, { useRef } from "react";
+import { useFullscreen } from "@/packages/hooks/useFullscreen/01/default-js/useFullscreen";
+import Button from "@/ui/Button";
+
+export default function UseFullscreenExample() {
+  const { isFullscreen, requestFullscreen, exitFullscreen } = useFullscreen();
+  const elementRef = useRef(null);
+
+  const handleEnterFullscreen = () => {
+    if (elementRef.current) {
+      requestFullscreen(elementRef.current);
+    }
+  };
+
+  return (
+    <div className="w-full h-80 flex flex-col justify-center items-center gap-8 text-foreground">
+      <div
+        ref={elementRef}
+        className="w-1/2 min-w-60 h-60 bg-secondary text-secondary-foreground flex flex-col justify-center items-center text-center gap-6"
+      >
+        <p>This content can be viewed in full-screen mode.</p>
+        <p>Fullscreen status: {isFullscreen ? "Enabled" : "Disabled"}</p>
+        <Button onPress={exitFullscreen}>Exit Fullscreen</Button>
+      </div>
+      <div className="flex gap-4">
+        <Button onPress={handleEnterFullscreen}>Enter Fullscreen</Button>
+      </div>
+    </div>
+  );
+}
---
+++ apps/registry/src/packages/hooks/useFullscreen/01/default-js/useFullscreen.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState, useCallback, useEffect } from "react";
+
+export const useFullscreen = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const requestFullscreen = useCallback((element) => {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    }
+    // This is for Safari
+    else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    }
+    // This is for IE11
+    else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+  }, []);
+
+  const exitFullscreen = useCallback(() => {
+    if (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.msFullscreenElement
+    ) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+      // This is for Safari
+      else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
+      // This is for IE11
+      else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+  }, []);
+
+  const handleFullscreenChange = useCallback(() => {
+    setIsFullscreen(!document.fullscreenElement);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+    document.addEventListener("webkitfullscreenchange", handleFullscreenChange);
+    document.addEventListener("msfullscreenchange", handleFullscreenChange);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+      document.removeEventListener(
+        "webkitfullscreenchange",
+        handleFullscreenChange
+      );
+      document.removeEventListener(
+        "msfullscreenchange",
+        handleFullscreenChange
+      );
+    };
+  }, [handleFullscreenChange]);
+
+  return {
+    isFullscreen,
+    requestFullscreen,
+    exitFullscreen,
+  };
+};
