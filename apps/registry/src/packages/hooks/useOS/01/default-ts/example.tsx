70933c7cf837282f05d7d95f930122eada729978
krishpatel023 <krishpatel0234@gmail.com>
2024-07-24 02:01:15 +0530

    registry/hooks/add: useOS (#56)

---
+++ apps/registry/src/packages/hooks/useOS/01/default-ts/example.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { useOS } from "@/packages/hooks/useOS/01/default-ts/useOS";
+
+export default function UseOSExample() {
+  const os = useOS();
+
+  return (
+    <div className="w-full h-60 flex flex-col justify-center items-center text-foreground">
+      <p>Your operating system is: {os}</p>
+    </div>
+  );
+}
