614dbf0774ff28c51db3e34197b261e251ad2b05
krishpatel023 <krishpatel0234@gmail.com>
2024-07-24 02:30:22 +0530

    registry/hooks/add: useProcess (#56)

---
+++ apps/registry/src/packages/hooks/useProcess/01/default-js/useProcess.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useCallback, useState } from "react";
+
+const useProcess = () => {
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const executeProcess = useCallback(async (callback) => {
+    try {
+      setIsProcessing(true);
+      await callback();
+    } catch (error) {
+      console.error("Error executing process:", error);
+    } finally {
+      setIsProcessing(false);
+    }
+  }, []);
+
+  return { isProcessing, executeProcess };
+};
+
+export default useProcess;
