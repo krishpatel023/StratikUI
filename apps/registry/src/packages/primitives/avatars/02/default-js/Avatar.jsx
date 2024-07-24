56929db41dd11b45b618042e0c2cd4e1e7ab3de5
krishpatel023 <krishpatel0234@gmail.com>
2024-07-24 18:15:49 +0530

    registry/primitives/add: avatars 02 (#56)

---
+++ apps/registry/src/packages/primitives/avatars/02/default-js/Avatar.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image, { StaticImageData } from "next/image";
+import { twMerge } from "tailwind-merge";
+
+export function Avatar({ src, alt, name, className, isDisabled }) {
+  const initials = name
+    .split(" ")
+    .map((word) => word.charAt(0))
+    .join("")
+    .substring(0, 2);
+
+  return (
+    <div
+      className={twMerge(
+        "size-12 rounded-full flex justify-center items-center group aria-disabled: bg-gradient-to-r from-blue-500 to-purple-500",
+        className
+      )}
+      aria-disabled={isDisabled}
+      aria-label="avatar"
+    >
+      {src ? (
+        <Image
+          src={src}
+          alt={alt || "avatar"}
+          className="rounded-[inherit] size-full"
+        />
+      ) : (
+        <h1 aria-label="initials" className="text-2xl text-primary-foreground">
+          {initials}
+        </h1>
+      )}
+    </div>
+  );
+}
+
+export function AvatarWithIcon({ icon, ...props }) {
+  return (
+    <div className="relative w-max">
+      <Avatar {...props} />
+      {icon && (
+        <div
+          className={twMerge(
+            "absolute -right-1 bottom-0 w-6 h-6 rounded-full flex justify-center items-center bg-primary-foreground"
+          )}
+        >
+          {icon}
+        </div>
+      )}
+    </div>
+  );
+}
