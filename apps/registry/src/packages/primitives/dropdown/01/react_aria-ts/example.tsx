215c2dcbd554800fdd4abc2ba6dfe0a206dd275a
krishpatel023 <krishpatel0234@gmail.com>
2024-07-25 01:39:52 +0530

    registry/primitives/add: dropdown 01 (#56)

---
+++ apps/registry/src/packages/primitives/dropdown/01/react_aria-ts/example.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Button from "@/ui/Button";
+import {
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  DropdownTrigger,
+  SubmenuTrigger,
+} from "@/packages/primitives/dropdown/01/react_aria-ts/Dropdown";
+import { Section } from "react-aria-components";
+import { IconProps } from "@/utils/types";
+
+export default function DropdownImplementation() {
+  return (
+    <DropdownTrigger
+      label="User Settings"
+      description="This is an user settings dropdown"
+    >
+      <Button>Settings</Button>
+      <Dropdown className="min-w-60">
+        <Section>
+          <DropdownHeader>My Account</DropdownHeader>
+          <DropdownItem>Profile</DropdownItem>
+          <DropdownItem isDisabled>Billing</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+        </Section>
+        <DropdownDivider />
+        <Section>
+          <DropdownHeader className="mt-2">Other Settings</DropdownHeader>
+
+          <SubmenuTrigger>
+            <DropdownItem className="flex items-center justify-between">
+              <span>Team</span>
+              <Icons.chevron className="h-5 w-5" />
+            </DropdownItem>
+
+            <Dropdown className="min-w-40">
+              <Section>
+                <DropdownHeader>Team</DropdownHeader>
+              </Section>
+              <DropdownDivider />
+              <Section>
+                <DropdownItem>Team 1</DropdownItem>
+                <DropdownItem>Team 2</DropdownItem>
+                <DropdownItem>Team 3</DropdownItem>
+              </Section>
+              <DropdownDivider />
+              <DropdownItem>Add Team</DropdownItem>
+            </Dropdown>
+          </SubmenuTrigger>
+
+          <DropdownItem>Support</DropdownItem>
+        </Section>
+        <DropdownDivider />
+        <DropdownItem className="hover:bg-error focus:bg-error">
+          Logout
+        </DropdownItem>
+      </Dropdown>
+    </DropdownTrigger>
+  );
+}
+
+const Icons = {
+  chevron: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8L6.22 5.28a.75.75 0 0 1 0-1.06"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  ),
+};
