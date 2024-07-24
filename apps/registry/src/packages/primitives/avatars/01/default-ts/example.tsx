d2982ced9b979796581bea6d54c5a70c35d778ed
krishpatel023 <krishpatel0234@gmail.com>
2024-07-24 18:02:20 +0530

    registry/primitives/add: avatars 01 (#56)

---
+++ apps/registry/src/packages/primitives/avatars/01/default-ts/example.tsx
@@ -0,0 +1,16 @@
+import ArrowHeading from "@/ui/ArrowHeading";
+import { Avatar } from "@/packages/primitives/avatars/01/default-ts/Avatar";
+import Dummy from "@/assets/Images/Image_2.jpg";
+
+export default function AvatarImplementation() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4">
+      {/* With Image */}
+      <ArrowHeading text="With Image" />
+      <Avatar src={Dummy} alt="Profile Picture" name="Profile Picture" />
+      {/* Without Image */}
+      <ArrowHeading text="Fallback / Without Image" />
+      <Avatar alt="Profile Picture" name="Profile Picture" />
+    </div>
+  );
+}
