367d92ed7b144095cfbd518401af60bc7955854b
krishpatel023 <krishpatel0234@gmail.com>
2024-07-25 01:55:00 +0530

    registry/primitives/add: header-blocks 01 (#56)

---
+++ apps/registry/src/packages/primitives/header-blocks/01/default-js/example.jsx
@@ -0,0 +1,42 @@
+import {
+  Header,
+  HeaderDropdown,
+  HeaderItem,
+} from "@/packages/primitives/header-blocks/01/default-js/HeaderBlocks";
+
+export default function HeaderBlocksImplementation() {
+  return (
+    <div className="w-full min-h-[25rem] flex flex-col justify-center items-center">
+      <Header className="flex justify-center items-center gap-4 h-16 border border-outline px-10 rounded-lg">
+        <HeaderItem className="hover:text-accent">Home</HeaderItem>
+        <HeaderItem className="hover:text-accent">Pricing</HeaderItem>
+        <HeaderItem className="hover:text-accent">
+          Products
+          <HeaderDropdown className="flex flex-col items-start">
+            <span>View Best Sellers</span>
+            <span>Electronics</span>
+            <span>Clothing</span>
+            <span>Books</span>
+          </HeaderDropdown>
+        </HeaderItem>
+        <HeaderItem className="hover:text-accent">
+          Services
+          <HeaderDropdown className="flex flex-col items-start">
+            <span>Web Design</span>
+            <span>Web Development</span>
+            <span>Mobile Development</span>
+            <span>Other Services</span>
+          </HeaderDropdown>
+        </HeaderItem>
+        <HeaderItem className="hover:text-accent">
+          About
+          <HeaderDropdown className="flex flex-col items-start">
+            <span>About Us</span>
+            <span>Careers</span>
+            <span>Contact Us</span>
+          </HeaderDropdown>
+        </HeaderItem>
+      </Header>
+    </div>
+  );
+}
