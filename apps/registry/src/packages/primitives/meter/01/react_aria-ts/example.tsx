e6ab8ce11f1d093c44bec581b327b65267d32385
krishpatel023 <krishpatel0234@gmail.com>
2024-07-25 03:04:31 +0530

    registry/primitives/add: meter 01 (#56)

---
+++ apps/registry/src/packages/primitives/meter/01/react_aria-ts/example.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState } from "react";
+import { Meter } from "@/packages/primitives/meter/01/react_aria-ts/Meter";
+import { Label } from "react-aria-components";
+import Button from "@/ui/Button";
+
+export default function MeterImplementation() {
+  const [value, setValue] = useState(50);
+
+  function Increment() {
+    if (value > 75) return;
+    setValue(value + 25);
+  }
+
+  function Decrement() {
+    if (value < 25) return;
+    setValue(value - 25);
+  }
+
+  return (
+    <div className="grid grid-cols-1 @md:grid-cols gap-4">
+      <div className="w-60 @md:w-80 flex flex-col gap-8 text-foreground">
+        <Meter value={value}>
+          <Label>Primary</Label>
+        </Meter>
+        <Meter value={value} variant="destructive">
+          <Label>Destructive</Label>
+        </Meter>
+        <Meter value={value} variant="outline">
+          <Label>Outline</Label>
+        </Meter>
+        <Meter value={value} variant="accent">
+          <Label>Accent</Label>
+        </Meter>
+
+        <div className="flex gap-4 items-center justify-center">
+          <Button onPress={Decrement}>Decrement</Button>
+          <Button onPress={Increment}>Increment</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
