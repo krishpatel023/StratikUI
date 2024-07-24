aba9e605e7fd48a2dc84e31000f08bf3bbb805c6
krishpatel023 <krishpatel0234@gmail.com>
2024-07-24 18:36:01 +0530

    registry/primitives/add: avatars 03 (#56)

---
+++ apps/registry/src/packages/primitives/avatars/03/default-ts/Avatar.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Image, { StaticImageData } from "next/image";
+import { twMerge } from "tailwind-merge";
+
+export interface AvatarProps {
+  src?: string | StaticImageData;
+  alt?: string;
+  className?: string;
+  name: string;
+  isDisabled?: boolean;
+}
+
+export function Avatar({ src, alt, name, className, isDisabled }: AvatarProps) {
+  const initials = name
+    .split(" ")
+    .map((word) => word.charAt(0))
+    .join("")
+    .substring(0, 2);
+
+  return (
+    <div
+      className={twMerge(
+        "size-12 rounded-full flex justify-center items-center group aria-disabled: bg-gradient-to-r from-blue-500 to-purple-500",
+        className
+      )}
+      aria-disabled={isDisabled}
+      aria-label="avatar"
+    >
+      {src ? (
+        <Image
+          src={src}
+          alt={alt || "avatar"}
+          className="rounded-[inherit] size-full"
+        />
+      ) : (
+        <h1 aria-label="initials" className="text-2xl text-primary-foreground">
+          {initials}
+        </h1>
+      )}
+    </div>
+  );
+}
+
+export interface AvatarStackProps {
+  avatars: AvatarProps[];
+  limit?: number;
+  withExtension?: boolean;
+}
+export function AvatarStack({
+  avatars,
+  limit = 5,
+  withExtension,
+}: AvatarStackProps) {
+  const displayedAvatars = avatars.slice(0, limit);
+  const remainingCount = avatars.length - limit;
+
+  return (
+    <div className="flex flex-row-reverse">
+      {withExtension && remainingCount > 0 && (
+        <div className="z-10 -ml-4 last:ml-0">
+          <Avatar name={`+ ${remainingCount}`} />
+        </div>
+      )}
+      {displayedAvatars.map((avatar, index) => (
+        <div
+          key={index}
+          className="-ml-4 last:ml-0"
+          style={{ zIndex: displayedAvatars.length - index }}
+        >
+          <Avatar {...avatar} />
+        </div>
+      ))}
+    </div>
+  );
+}
