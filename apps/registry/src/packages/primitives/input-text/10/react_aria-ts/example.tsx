b224b1a793ce2bb54e8c63d2af7141870dd1ccde
krishpatel023 <krishpatel0234@gmail.com>
2024-07-25 22:12:42 +0530

    registry/primitives/add: input-text 10 (#56)

---
+++ apps/registry/src/packages/primitives/input-text/10/react_aria-ts/example.tsx
@@ -0,0 +1,47 @@
+import ArrowHeading from "@/ui/ArrowHeading";
+import { Input } from "@/packages/primitives/input-text/10/react_aria-ts/Input";
+
+export default function StatefulLabelCoveredInputComponent() {
+  return (
+    <div className="w-full flex flex-col items-center gap-6">
+      <span>
+        <ArrowHeading text="Default" className="mb-2" />
+        <Input name="Name" type="text" placeholder="Name" label="Name" />
+      </span>
+
+      <span>
+        <ArrowHeading text="Error" className="mb-2" />
+        <Input
+          name="Name"
+          type="text"
+          label="Name"
+          placeholder="Name"
+          state="isInvalid"
+        />
+      </span>
+
+      <span>
+        <ArrowHeading text="Disabled" className="mb-2" />
+        <Input
+          name="Name"
+          type="text"
+          label="Name"
+          placeholder="Name"
+          state="isDisabled"
+        />
+      </span>
+
+      <span>
+        <ArrowHeading text="ReadOnly" className="mb-2" />
+        <Input
+          name="Name"
+          type="text"
+          label="Name"
+          placeholder="Name"
+          isReadOnly
+          value={"John Doe"}
+        />
+      </span>
+    </div>
+  );
+}
