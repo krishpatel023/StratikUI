c5785f7fd33b94b88cdace99ab0d4f52dcfefd83
krishpatel023 <krishpatel0234@gmail.com>
2024-07-25 14:39:02 +0530

    registry/primitives/add: input-number 02 (#56)

---
+++ apps/registry/src/packages/primitives/input-number/02/react_aria-ts/example.tsx
@@ -0,0 +1,34 @@
+import ArrowHeading from "@/ui/ArrowHeading";
+import { InputNumber } from "@/packages/primitives/input-number/02/react_aria-ts/InputNumber";
+
+export default function InputImplementation() {
+  return (
+    <div className="w-full flex flex-col justify-center items-center gap-8">
+      <ArrowHeading text="Default Usage" />
+      <InputNumber
+        placeholder="Enter a value"
+        label="Label"
+        description="This is a description"
+      />
+      <ArrowHeading text="Default Usage with showButtons false" />
+      <InputNumber
+        placeholder="Enter a value"
+        label="Label"
+        description="This is a description"
+        showButtons={false}
+      />
+      <ArrowHeading text=" Usage with different format." />
+      <InputNumber
+        placeholder="Enter a value"
+        label="Price"
+        description="Choose the amount you want to pay"
+        formatOptions={{
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        }}
+      />
+    </div>
+  );
+}
