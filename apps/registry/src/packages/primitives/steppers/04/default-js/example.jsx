91cf5c6f502156721bbb9a5168592d9eed67ed4a
krishpatel023 <krishpatel0234@gmail.com>
2024-07-26 02:49:45 +0530

    registry/primitives/add: stepper 04 (#56)

---
+++ apps/registry/src/packages/primitives/steppers/04/default-js/example.jsx
@@ -0,0 +1,104 @@
+"use client";
+
+import {
+  Stepper,
+  Step,
+} from "@/packages/primitives/steppers/04/default-js/Stepper";
+import Button from "@/ui/Button";
+import { useState } from "react";
+
+export const StepperImplementation = () => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handlePrev = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-10">
+      <Stepper currentStep={activeStep} onChange={handleStepChange}>
+        <Step>
+          <div>
+            <h1 className="text-lg">Login</h1>
+            <p className="text-secondary-foreground">
+              You need to login to you account
+            </p>
+            <span className="group-data-[state='incomplete']:block hidden text-error">
+              Incomplete
+            </span>
+            <span className="group-data-[state='complete']:block hidden text-success">
+              Complete
+            </span>
+            <span className="group-data-[state='active']:block hidden text-accent">
+              Active
+            </span>
+          </div>
+        </Step>
+        <Step>
+          <div>
+            <h1 className="text-lg">Add your wallet</h1>
+            <p className="text-secondary-foreground">
+              Add your wallet to your account
+            </p>
+            <span className="group-data-[state='incomplete']:block hidden text-error">
+              Incomplete
+            </span>
+            <span className="group-data-[state='complete']:block hidden text-success">
+              Complete
+            </span>
+            <span className="group-data-[state='active']:block hidden text-accent">
+              Active
+            </span>
+          </div>
+        </Step>
+        <Step>
+          <div>
+            <h1 className="text-lg">Verify the plan</h1>
+            <p className="text-secondary-foreground">
+              Verify the plan you want to subscribe
+            </p>
+            <span className="group-data-[state='incomplete']:block hidden text-error">
+              Incomplete
+            </span>
+            <span className="group-data-[state='complete']:block hidden text-success">
+              Complete
+            </span>
+            <span className="group-data-[state='active']:block hidden text-accent">
+              Active
+            </span>
+          </div>
+        </Step>
+        <Step>
+          <div>
+            <h1 className="text-lg">Purchase</h1>
+            <p className="text-secondary-foreground">
+              Make the purchase using multiple payment methods
+            </p>
+            <span className="group-data-[state='incomplete']:block hidden text-error">
+              Incomplete
+            </span>
+            <span className="group-data-[state='complete']:block hidden text-success">
+              Complete
+            </span>
+            <span className="group-data-[state='active']:block hidden text-accent">
+              Active
+            </span>
+          </div>
+        </Step>
+      </Stepper>
+      <span className="text-foreground">Current step: {activeStep}</span>
+      <div className="flex gap-4">
+        <Button onPress={handlePrev}>Prev</Button>
+        <Button onPress={handleNext}>Next</Button>
+      </div>
+    </div>
+  );
+};
