2c8c14f5f889102fe81c771164a3b92909f4fa59
krishpatel023 <krishpatel0234@gmail.com>
2024-07-28 00:48:39 +0530

    registry/primitives/add: text 01 (#56)

---
+++ apps/registry/src/packages/primitives/text/01/react_aria-js/example.jsx
@@ -0,0 +1,21 @@
+import ArrowHeading from "@/ui/ArrowHeading";
+import { GradientText } from "@/packages/primitives/text/01/react_aria-js/Text";
+
+export default function GradientTextImplementation() {
+  return (
+    <div className="w-80 @lg:w-[40rem] flex flex-col gap-4">
+      <ArrowHeading text="Default" />
+      <GradientText className="bg-gradient-to-br text-3xl @lg:text-7xl font-bold">
+        Bring Gradients to your Text
+      </GradientText>
+      <ArrowHeading text="Triple Gradient" />
+      <GradientText className="from-blue-600 via-green-500 to-indigo-400 text-3xl @lg:text-7xl font-bold">
+        Bring Gradients to your Text
+      </GradientText>
+      <ArrowHeading text="In Different Direction" />
+      <GradientText className="bg-gradient-to-b from-blue-700 to-indigo-400 text-3xl @lg:text-7xl font-bold">
+        Bring Gradients to your Text
+      </GradientText>
+    </div>
+  );
+}
