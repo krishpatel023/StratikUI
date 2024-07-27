447c1da0ce2ff6e541f1588fbd033d8e11942d97
krishpatel023 <krishpatel0234@gmail.com>
2024-07-28 00:59:46 +0530

    registry/primitives/add: tthem toggles 01 (#56)

---
+++ apps/registry/src/packages/primitives/theme-toggles/01/react_aria-ts/example.tsx
@@ -0,0 +1,9 @@
+import { ThemeToggle } from "@/packages/primitives/theme-toggles/01/react_aria-ts/ThemeToggle";
+
+export default function ThemeToggleImplementation() {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <ThemeToggle />
+    </div>
+  );
+}
