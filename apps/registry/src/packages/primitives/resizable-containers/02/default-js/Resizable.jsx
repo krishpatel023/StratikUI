1586941a23115530543a641198b569fde62f724b
krishpatel023 <krishpatel0234@gmail.com>
2024-07-27 15:45:14 +0530

    registry/primitives/add: resizable-containers 02 (#56)

---
+++ apps/registry/src/packages/primitives/resizable-containers/02/default-js/Resizable.jsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+export function ResizeBoundingElement({ containerRef, className, children }) {
+  return (
+    <div ref={containerRef} className={twMerge("w-full", className)}>
+      {children}
+    </div>
+  );
+}
+
+export function ResizableContainer({
+  children,
+  minWidth,
+  minHeight,
+  direction,
+  handleResize,
+  resizableRef,
+  className,
+}) {
+  return (
+    <div className="w-max">
+      <Handle
+        direction="top"
+        directionArr={direction}
+        handleResize={handleResize}
+      />
+      <div className="h-full flex">
+        <Handle
+          direction="left"
+          directionArr={direction}
+          handleResize={handleResize}
+        />
+        <div
+          ref={resizableRef}
+          style={{
+            minWidth: `${minWidth}px`,
+            minHeight: `${minHeight}px`,
+          }}
+          className={className}
+        >
+          {children}
+        </div>
+        <Handle
+          direction="right"
+          directionArr={direction}
+          handleResize={handleResize}
+        />
+      </div>
+      <Handle
+        direction="bottom"
+        directionArr={direction}
+        handleResize={handleResize}
+      />
+    </div>
+  );
+}
+
+export function Handle({ direction, directionArr, handleResize }) {
+  return (
+    <>
+      {directionArr.includes(direction) && (
+        <>
+          {(direction === "right" || direction === "left") && (
+            <div
+              className="w-4 flex justify-center items-center cursor-col-resize z-10"
+              aria-label={`Resize ${direction}`}
+              onMouseDown={(e) => handleResize(e, direction)}
+              onTouchStart={(e) => handleResize(e, direction)}
+            >
+              <div className="min-h-8 bg-foreground rounded-full min-w-[0.25rem]"></div>
+            </div>
+          )}
+          {(direction === "top" || direction === "bottom") && (
+            <div
+              className="h-4 flex justify-center items-center cursor-row-resize z-10"
+              aria-label={`Resize ${direction}`}
+              onMouseDown={(e) => handleResize(e, direction)}
+              onTouchStart={(e) => handleResize(e, direction)}
+            >
+              <div className="min-w-8 bg-foreground rounded-full min-h-[0.25rem]"></div>
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
+}
---
+++ apps/registry/src/packages/primitives/resizable-containers/02/default-js/example.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import useResizable from "@/packages/hooks/useResizable/01/default-js/useResizable";
+import {
+  ResizableContainer,
+  ResizeBoundingElement,
+} from "@/packages/primitives/resizable-containers/02/default-js/Resizable";
+import { useRef } from "react";
+
+export default function ResizeImplementation() {
+  const containerRef = useRef(null);
+  const resizableRef = useRef(null);
+
+  const options = {
+    minWidth: "100px",
+    minHeight: "100px",
+    maxWidth: "70%",
+    maxHeight: "100%",
+    expandBoundingElement: false,
+  };
+
+  const { handleResize } = useResizable(containerRef, resizableRef, options);
+
+  return (
+    <div className="flex justify-center items-center gap-4 w-full ">
+      <ResizeBoundingElement
+        containerRef={containerRef}
+        className="h-80 w-[35rem] max-w-[35rem] bg-accent flex"
+      >
+        <ResizableContainer
+          minWidth={100}
+          minHeight={100}
+          direction={["right"]}
+          className="h-full bg-secondary"
+          handleResize={handleResize}
+          resizableRef={resizableRef}
+        >
+          <div className="text-foreground h-80 text-center flex justify-center items-center">
+            <h1>This is a resizable container</h1>
+          </div>
+        </ResizableContainer>
+        <div className="w-full h-full bg-secondary text-secondary-foreground text-center flex justify-center items-center px-2">
+          <h1>
+            This is not a resizable div but will take the remaining width in the
+            container.
+          </h1>
+        </div>
+      </ResizeBoundingElement>
+    </div>
+  );
+}
