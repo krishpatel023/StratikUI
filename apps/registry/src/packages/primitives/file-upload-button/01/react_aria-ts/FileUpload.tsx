940f63b1325246c090ce71f6d592d0502c82e2bb
krishpatel023 <krishpatel0234@gmail.com>
2024-07-29 23:22:53 +0530

    registry/primitive/add: file upload button (#56)

---
+++ apps/registry/src/packages/primitives/file-upload-button/01/react_aria-ts/FileUpload.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import {
+  Button,
+  ButtonProps,
+} from "@/packages/primitives/buttons/01/react_aria-ts/Button";
+import { FileTrigger, FileTriggerProps } from "react-aria-components";
+
+interface FileUploadButtonProps extends FileTriggerProps {
+  className?: string;
+  getFiles?: (files: File[]) => void;
+  variant?: ButtonProps["variant"];
+}
+
+export function FileUploadButton({
+  className,
+  children,
+  getFiles,
+  variant = "primary",
+  ...props
+}: FileUploadButtonProps) {
+  function handleFileUpload(e: FileList | null) {
+    let files = Array.from(e as FileList);
+    getFiles && getFiles(files);
+  }
+
+  return (
+    <FileTrigger onSelect={(e) => handleFileUpload(e)} {...props}>
+      <Button className={className} variant={variant}>
+        {children}
+      </Button>
+    </FileTrigger>
+  );
+}
