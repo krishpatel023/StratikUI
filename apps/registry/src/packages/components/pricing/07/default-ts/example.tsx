2eccb3662cdbdd9ae80fc8a46a820f78737b4b2b
krishpatel023 <krishpatel0234@gmail.com>
2024-08-01 02:00:02 +0530

    registry/components/add: pricing 07 (#56)

---
+++ apps/registry/src/packages/components/pricing/07/default-ts/example.tsx
@@ -0,0 +1,15 @@
+import { Pricing } from "@/packages/components/pricing/05/default-ts/Pricing";
+import { PricingGrid } from "@/packages/components/pricing/07/default-ts/Pricing";
+
+export default function PricingImplementation() {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <div className="hidden @lg:block w-[90%] mt-40 mb-20">
+        <PricingGrid />
+      </div>
+      <div className="@lg:hidden w-[90%] mt-40 mb-20">
+        <Pricing />
+      </div>
+    </div>
+  );
+}
