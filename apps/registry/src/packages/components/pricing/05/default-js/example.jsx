42854a8ebec1341438bb2aee9eb32a70278431d9
krishpatel023 <krishpatel0234@gmail.com>
2024-08-01 01:22:24 +0530

    registry/components/add: pricing 05 (#56)

---
+++ apps/registry/src/packages/components/pricing/05/default-js/example.jsx
@@ -0,0 +1,9 @@
+import { Pricing } from "@/packages/components/pricing/05/default-js/Pricing";
+
+export default function PricingImplementation() {
+  return (
+    <div className="w-full min-h-[35rem] flex justify-center items-center py-10">
+      <Pricing />
+    </div>
+  );
+}
