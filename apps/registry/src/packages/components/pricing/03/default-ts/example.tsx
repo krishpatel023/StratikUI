cb24014e793e15909c126f5ebc2eff20f18743a0
krishpatel023 <krishpatel0234@gmail.com>
2024-08-01 00:40:09 +0530

    registry/components/add: pricing 03 (#56)

---
+++ apps/registry/src/packages/components/pricing/03/default-ts/example.tsx
@@ -0,0 +1,9 @@
+import { Pricing } from "@/packages/components/pricing/03/default-ts/Pricing";
+
+export default function PricingImplementation() {
+  return (
+    <div className="w-full min-h-[35rem] flex justify-center items-center py-10">
+      <Pricing />
+    </div>
+  );
+}
