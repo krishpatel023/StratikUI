197f1a7867b622ed779232c30636eabbd628b72b
krishpatel023 <krishpatel0234@gmail.com>
2024-07-31 22:16:33 +0530

    registry/components/add: authentication 10 (#56)

---
+++ apps/registry/src/packages/components/authentication/10/default-ts/example.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@/packages/components/authentication/10/default-ts/SignUp";
+
+export default function AuthenticationImplementation() {
+  return (
+    <div className="w-full">
+      <SignUp />
+    </div>
+  );
+}
