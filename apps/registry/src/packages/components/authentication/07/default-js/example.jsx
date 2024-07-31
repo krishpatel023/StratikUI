266a928cfd8dfdc082b9bdb743275f1770a9a6b3
krishpatel023 <krishpatel0234@gmail.com>
2024-07-31 18:58:36 +0530

    registry/components/add: authentication 07 (#56)

---
+++ apps/registry/src/packages/components/authentication/07/default-js/example.jsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@/packages/components/authentication/07/default-js/SignUp";
+
+export default function AuthenticationImplementation() {
+  return (
+    <div className="w-full">
+      <SignUp />
+    </div>
+  );
+}
