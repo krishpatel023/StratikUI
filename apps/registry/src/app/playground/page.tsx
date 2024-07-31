c98993a6acaf18ad6bdc10197fc6f7af38b276e9
krishpatel023 <krishpatel0234@gmail.com>
2024-07-31 16:59:29 +0530

    regidtry/fix: playground (#53)
    Added display component to the playground for easy testing. It also avoids confusion as I have merged the resizable display and display into one. Now it is controlled by the category prop which makes it easy for contributors to understand

--- apps/registry/src/app/playground/page.tsx
+++ apps/registry/src/app/playground/page.tsx
@@ -1,39 +1,36 @@
-import { twMerge } from "tailwind-merge";
+import Display, { DisplayProps } from "@/ui/Display";
 
 export default function Page() {
   return (
-    <Playground>
-      {/* Remove this div and add your component here */}
-      <div className="min-h-60 flex justify-center items-center">
-        <h1 className="text-foreground text-center">Playground</h1>
-      </div>
+    <Playground category="hooks">
+      {/* Remove this h1 and add your component here */}
+      <h1 className="text-2xl mx-auto text-foreground">Playground</h1>
     </Playground>
   );
 }
 
 export function Playground({
   children,
-  className,
+  category,
 }: {
   children?: React.ReactNode;
-  className?: string;
+  category: DisplayProps["category"];
 }) {
   return (
-    <div>
-      <h1 className="text-foreground mt-20 text-center">
+    <>
+      <h1 className="text-foreground mt-20 text-center text-xl underline underline-offset-2">
         Welcome to the Playground
       </h1>
+      <h2 className="text-secondary-foreground max-w-[50rem] px-4 mx-auto pt-4 text-center">
+        <span className="underline underline-offset-2">NOTE:</span> Adjust the
+        category of the Playground component. The primitives and hooks have
+        different display layout than the components. Make sure to adjust the
+        category to the correct one.
+      </h2>
       <h2 className="text-secondary-foreground mt-2 mb-10 text-center">
         Please test the components here and DO NOT PUSH this page to production
       </h2>
-      <div
-        className={twMerge(
-          "@container w-[90%] mx-auto h-max flex flex-col justify-center items-center border border-outline-secondary rounded-lg",
-          className
-        )}
-      >
-        {children}
-      </div>
-    </div>
+      <Display category={category}>{children}</Display>
+    </>
   );
 }
