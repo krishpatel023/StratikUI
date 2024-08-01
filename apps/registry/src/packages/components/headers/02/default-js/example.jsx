d94848d6a974987c1aafd8fc05238450273c1548
krishpatel023 <krishpatel0234@gmail.com>
2024-08-01 17:23:27 +0530

    registry/components/add: headers 02 (#56)

---
+++ apps/registry/src/packages/components/headers/02/default-js/example.jsx
@@ -0,0 +1,12 @@
+import { HeaderComponent } from "@/packages/components/headers/02/default-js/Header";
+
+export default function HeaderImplementation() {
+  return (
+    <div className="w-full relative overflow-hidden">
+      <HeaderComponent />
+      <div className="min-h-[40rem] w-full text-center flex justify-center items-center">
+        <h1 className="text-2xl text-foreground">Content</h1>
+      </div>
+    </div>
+  );
+}
