4bd88f0a5d0c2e713d6ce52a98d4b90d03f4399e
krishpatel023 <krishpatel0234@gmail.com>
2024-08-02 22:59:17 +0530

    registry/components/add: footer 02 (#56)

---
+++ apps/registry/src/packages/components/footer/02/default-js/Footer.jsx
@@ -0,0 +1,15 @@
+import { ThemeToggle } from "@/packages/primitives/theme-toggles/01/react_aria-js/ThemeToggle";
+
+export function Footer() {
+  return (
+    <div className="w-full h-60  @md:h-40 flex flex-col @md:flex-row justify-center @md:justify-between gap-8 items-center px-16">
+      <div className="flex flex-col justify-center @md:items-start items-center text-center text-foreground">
+        <h1 className="text-xl font-semibold">Stratik / UI</h1>
+        <span>© 2024 Stratik UI. All rights reserved.</span>
+      </div>
+      <div>
+        <ThemeToggle />
+      </div>
+    </div>
+  );
+}
