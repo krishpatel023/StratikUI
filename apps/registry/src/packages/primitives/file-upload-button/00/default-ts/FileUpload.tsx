7c7fe4b142d3d0f369fdb795df6254ecf51c76db
krishpatel023 <krishpatel0234@gmail.com>
2024-08-03 12:38:30 +0530

    registry/primitives/add: file upload button (#56)

---
+++ apps/registry/src/packages/primitives/file-upload-button/00/default-ts/FileUpload.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  useRef,
+} from "react";
+
+export interface FileUploadButtonProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  getFiles?: (files: File[]) => void;
+  allowsMultiple?: boolean;
+  acceptedFileTypes?: InputHTMLAttributes<HTMLInputElement>["accept"][];
+}
+
+export function FileUploadButton({
+  children,
+  getFiles,
+  allowsMultiple,
+  acceptedFileTypes,
+  className,
+  ...props
+}: FileUploadButtonProps) {
+  function handleFileUpload(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    const target = e.target as HTMLInputElement;
+
+    let Files: File[] = [];
+
+    if (allowsMultiple) {
+      Files = Array.from(target.files ? target.files : []);
+    } else {
+      const file = target.files?.[0];
+      Files = file ? [file] : [];
+    }
+    getFiles && getFiles(Files);
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleClick() {
+    inputRef.current?.click();
+  }
+
+  return (
+    <>
+      <input
+        type="file"
+        ref={inputRef}
+        className="hidden"
+        onChange={(e) => handleFileUpload(e)}
+        multiple={allowsMultiple}
+        accept={acceptedFileTypes?.join(",")}
+        {...props}
+      />
+      <button className={className} onClick={handleClick}>
+        {children}
+      </button>
+    </>
+  );
+}
