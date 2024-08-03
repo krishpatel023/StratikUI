37356e856b1587a2e74c6eb7730c1d056e35a8dc
krishpatel023 <krishpatel0234@gmail.com>
2024-08-03 10:38:45 +0530

    registry/components/add: banners 01 (#56)

---
+++ apps/registry/src/packages/components/banners/01/default-ts/example.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { Banner } from "./Banner";
+import { GradientBackground } from "@/packages/primitives/containers/01/default-ts/Container";
+import { Button } from "@/packages/primitives/buttons/02/default-ts/Button";
+
+export default function BannerImplementation() {
+  const [open, setOpen] = useState(true);
+  return (
+    <div className="w-full relative">
+      <Banner
+        className="flex justify-center items-center h-16 py-2 px-6 bg-blur text-blur-foreground gap-8 border-b border-outline-secondary"
+        open={open}
+        type="fixed"
+      >
+        <h1 className="font-semibold">
+          Checkout the latest release of the components that are available for
+          free.
+        </h1>
+        <GradientBackground>
+          <button className="py-2 px-4 rounded-md bg-background text-foreground z-10 relative">
+            Get Started
+          </button>
+        </GradientBackground>
+      </Banner>
+      <div className="min-w-full min-h-[50rem] flex justify-center items-center">
+        <Button onClick={() => setOpen(!open)}>Toggle Banner</Button>
+      </div>
+      <Background />
+    </div>
+  );
+}
+
+const Background = () => {
+  return (
+    <div className="absolute top-0 left-0 -z-10 h-full w-full bg-white dark:bg-black">
+      <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] dark:bg-[rgba(73,89,123,0.5)] opacity-50 blur-[80px]"></div>
+    </div>
+  );
+};
