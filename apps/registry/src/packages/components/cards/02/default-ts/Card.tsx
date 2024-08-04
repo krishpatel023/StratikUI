b7d2a9996084a744910be055db97c89a9caa8c57
krishpatel023 <krishpatel0234@gmail.com>
2024-08-04 11:37:17 +0530

    registry/components/add: cards 02 (#56)

---
+++ apps/registry/src/packages/components/cards/02/default-ts/Card.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/packages/primitives/buttons/02/default-ts/Button";
+
+export function Card() {
+  return (
+    <div className="w-80 py-4 px-6 rounded-lg flex flex-col justify-center gap-3 bg-primary text-primary-foreground border shadow-sm border-outline-secondary">
+      <h1 className="text-2xl font-semibold">Title</h1>
+      <h3 className="text-secondary-foreground">
+        This is something great. Design this according to your needs and make it
+        look great.
+      </h3>
+      <Button>Get Started</Button>
+    </div>
+  );
+}
