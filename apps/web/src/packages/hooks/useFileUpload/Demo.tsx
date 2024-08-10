158fc2b7457feb57436396a177ec2509f28157a4
krishpatel023 <krishpatel0234@gmail.com>
2024-08-10 23:50:33 +0530

    web/delete: old docs (#56)

--- apps/web/src/packages/hooks/useFileUpload/Demo.tsx
+++
@@ -1,9 +0,0 @@
-import { FileUpload } from "@/packages/components/file_upload/File_Upload_1";
-
-export function Demo() {
-  return (
-    <div className="w-full flex justify-center items-center">
-      <FileUpload />
-    </div>
-  );
-}
--- apps/web/src/packages/hooks/useResizable/Demo.tsx
+++
@@ -1,3 +0,0 @@
-import { ResizeImplementation } from "@/packages/primitives/resizable_container/Resizable_1";
-
-export const Demo = ResizeImplementation;
