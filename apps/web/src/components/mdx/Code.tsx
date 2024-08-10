f89b536baa2a6ba5e146a9d31b934d8917d70240
krishpatel023 <krishpatel0234@gmail.com>
2024-08-10 14:58:52 +0530

    web/refactor: codeblock (#53)

--- apps/web/src/components/mdx/Code.tsx
+++ apps/web/src/components/mdx/Code.tsx
@@ -1,196 +1,297 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 import {
+  Section,
   Select,
-  SelectContent,
-  SelectGroup,
+  SelectHeader,
   SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
 } from "@/components/ui/Select";
 import { Icons } from "@/utils/icons";
-import StringCleaner from "@/scripts/StringCleaner";
-import CodeHighlight from "../ui/CodeHighlight";
-import { Code } from "@/utils/constants";
+import StringCleaner, { ColorPaletteSettings } from "@/utils/StringCleaner";
+import { Button, Key } from "react-aria-components";
 import { BundledLanguage } from "shiki/bundle/web";
+import { twMerge } from "tailwind-merge";
+import CodeHighlight from "../ui/CodeHighlight";
+import { Tooltip, TooltipTrigger } from "../ui/Tooltip";
 
-export const CodeBlock = ({ children }: { children: ReactNode }) => {
-  const processCodeBlocks = (codeBlocks: ReactNode[]): Code[] => {
-    const result: Code[] = [];
-    const codeBlocksByName: { [key: string]: Code } = {};
+export interface CodeIndividual {
+  provider: "default" | "react_aria";
+  type: "js" | "ts";
+  name: string;
+  language: BundledLanguage;
+  content: string;
+}
 
-    codeBlocks.forEach((codeBlock) => {
-      const { props } = codeBlock as {
+const ALLOWED_NOMENCLATURE = {
+  provider: ["default", "react_aria"],
+  type: ["js", "ts"],
+};
+
+export function CodeBlock({ children }: { children: ReactNode }) {
+  function checkNomenclature(provider: string, type: string): boolean {
+    if (ALLOWED_NOMENCLATURE.provider.includes(provider)) {
+      if (ALLOWED_NOMENCLATURE.type.includes(type)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function processCodeBlock(blocks: ReactNode[]): CodeIndividual[] {
+    console.log("blocks", blocks);
+    let result: CodeIndividual[] = [];
+    blocks.forEach((block) => {
+      const { props } = block as {
         props: { language: string; children: ReactNode };
       };
       const { language, children } = props;
-      const name = language.split("|").pop()?.replace(/"/g, "") || "";
 
-      if (!codeBlocksByName[name]) {
-        codeBlocksByName[name] = { name, content: [] };
-        result.push(codeBlocksByName[name]);
-      }
+      const languageArray = language.split("|");
+      const provider = languageArray[0];
+      const type = languageArray[1];
+      const fullName = languageArray[2];
 
-      const codeDestructor = (
-        children as { props: { children: string } }
-      ).props.children?.replace(/\\n$/, "");
+      const name = fullName.split(".")[0];
+      const extension = fullName.split(".")[1];
 
-      const code = codeDestructor ? codeDestructor : "";
+      const content = children as { props: { children: string } };
+      const code = content.props.children?.replace(/\\n$/, "");
 
-      const lang = language.split("|")[0] as BundledLanguage;
-      codeBlocksByName[name].content.push({ language: lang, code });
+      if (!checkNomenclature(provider, type)) return;
+
+      result.push({
+        provider: provider as CodeIndividual["provider"],
+        type: type as CodeIndividual["type"],
+        name: name,
+        language: extension as BundledLanguage,
+        content: code,
+      });
     });
-
     return result;
-  };
+  }
 
-  const result = processCodeBlocks(
-    Array.isArray(children) ? children : [children]
+  const result = processCodeBlock(
+    Array.isArray(children) ? children : ([children] as ReactNode[])
   );
 
-  return <>{result ? <CodeDisplay codeArray={result} /> : null}</>;
-};
+  console.log("result", result);
 
-const CodeDisplay = ({
+  return <>{result && <CodeDisplay codeArray={result} />}</>;
+}
+
+interface AvailableDataProps {
+  providers: CodeIndividual["provider"][];
+  types: CodeIndividual["type"][];
+}
+
+function CodeDisplay({
   codeArray,
   withCounter = true,
 }: {
-  codeArray: Code[];
+  codeArray: CodeIndividual[];
   withCounter?: boolean;
-}) => {
-  const [activeCode, setActiveCode] = useState<number>(0);
-  const [activeLanguage, setActiveLanguage] = useState<number>(0);
+}) {
+  // Link this to the hook
+  const DEFAULT_PROVIDER = "default";
+  const DEFAULT_TYPE = "ts";
 
-  const [btnClick, setBtnClick] = useState(false);
-  const handleCopy = () => {
-    if (!codeArray[activeCode].content[activeLanguage].code) return;
-    navigator.clipboard.writeText(
-      codeArray[activeCode].content[activeLanguage].code
+  const [files, setFiles] = useState<CodeIndividual[]>([]);
+  const [availableData, setAvailableData] = useState<AvailableDataProps>({
+    providers: [],
+    types: [],
+  });
+
+  const [activeProvider, setActiveProvider] =
+    useState<CodeIndividual["provider"]>(DEFAULT_PROVIDER);
+  const [activeType, setActiveType] =
+    useState<CodeIndividual["type"]>(DEFAULT_TYPE);
+  const [activeFile, setActiveFile] = useState<CodeIndividual>(
+    files[0] || { name: "", content: [], provider: "", type: "", language: "" }
+  );
+  const [activeColorTheme, setActiveColorTheme] =
+    useState<ColorPaletteSettings>("tailwind");
+
+  function getAvailableData() {
+    const providers = Array.from(
+      new Set(codeArray.map((item) => item.provider))
     );
-    setBtnClick(true);
-    const time = setTimeout(handleCopyBtn, 1000);
-  };
-
-  function handleCopyBtn() {
-    setBtnClick(false);
+    const types = Array.from(new Set(codeArray.map((item) => item.type)));
+    setAvailableData({
+      providers,
+      types,
+    });
   }
-  // useEffect(() => {
-  //   // const index = getPreferenced("tsx");
-  //   // console.log("index", index);
 
-  //   setActiveLanguage(0);
-  // }, [activeCode]);
+  function filterByProvider(
+    provider: CodeIndividual["provider"],
+    files: CodeIndividual[]
+  ) {
+    return files.filter((item) => item.provider === provider);
+  }
 
-  // const getPreferenced = (pref: string) => {
-  //   const index = activeCode ? activeCode : 0;
-  //   // if (index === 0) return null;
-  //   for (let i = 0; i < codeArray[index].content.length; i++) {
-  //     if (pref === codeArray[index].content[i].language) {
-  //       return i;
-  //     }
-  //   }
-  //   return 0;
-  // };
+  function filterByType(type: CodeIndividual["type"], files: CodeIndividual[]) {
+    return files.filter((item) => item.type === type);
+  }
+
+  useEffect(() => {
+    getAvailableData();
+    const filteredByProvider = filterByProvider(activeProvider, codeArray);
+    const filteredByType = filterByType(activeType, filteredByProvider);
+    setFiles(filteredByType);
+    const activeFilePrev = filteredByType.find(
+      (item) => item.name === activeFile.name
+    );
+    setActiveFile(activeFilePrev || filteredByType[0]);
+    console.log("files", filteredByType);
+  }, [activeProvider, activeType]);
 
   return (
-    <>
-      {activeCode >= 0 && activeLanguage >= 0 ? (
-        <div className="w-full border-[2px] border-slate-100 dark:border-neutral-900/70 rounded-lg text-foreground p-0">
-          {/* TASKBAR */}
-          <div className="w-full h-8 flex justify-between items-center mt-2">
-            {/* BUTTONS */}
+    <div>
+      <div className="w-full border-[2px] border-slate-100 dark:border-neutral-900/70 rounded-lg text-foreground p-0">
+        {/* TASKBAR */}
+        <div className="w-full h-8 flex justify-between items-center mt-2">
+          {/* BUTTONS */}
 
-            <div className="hidden md:flex gap-2 px-4 h-full">
-              {codeArray?.map((item, i) => (
-                <button
-                  key={i}
-                  className={
-                    activeCode === i
-                      ? "text-sm h-full px-4 py-1 rounded-t-md dark:bg-neutral-900/70 bg-gray-50 border-t-2 border-x-2 dark:border-neutral-900 border-slate-100"
-                      : "text-sm h-full px-4 py-1"
-                  }
-                  onClick={() => setActiveCode(i)}
-                >
-                  {item.name}
-                </button>
-              ))}
-            </div>
-            <div className="block md:hidden h-full">
-              <Select
-                onValueChange={(e) => {
-                  setActiveCode(parseInt(e));
-                }}
+          <div className="hidden md:flex gap-2 px-4 h-full">
+            {files?.map((file, i) => (
+              <button
+                key={i}
+                className={
+                  activeFile.name === file.name
+                    ? "text-sm h-full px-4 py-1 rounded-t-md dark:bg-neutral-900/70 bg-gray-50 border-t-2 border-x-2 dark:border-neutral-900 border-slate-100"
+                    : "text-sm h-full px-4 py-1"
+                }
+                onClick={() => setActiveFile(file)}
               >
-                <SelectTrigger className="h-6 gap-2 border-none">
-                  <SelectValue placeholder={codeArray[activeCode].name} />
-                </SelectTrigger>
-                <SelectContent className="bg-background text-foreground">
-                  <SelectGroup>
-                    <SelectLabel>File</SelectLabel>
-                    {codeArray?.map((item, i) => (
-                      <SelectItem key={i} value={`${i}`}>
-                        {item.name}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            {/* OPERATOR */}
-            <div className="px-2 sm:px-4 flex pb-2 gap-1">
-              <Select
-                onValueChange={(e) => {
-                  setActiveLanguage(parseInt(e));
-                }}
-              >
-                <SelectTrigger className="h-6 gap-2 border-none">
-                  <SelectValue
-                    placeholder={
-                      codeArray[activeCode].content[activeLanguage].language
-                    }
-                  />
-                </SelectTrigger>
-                <SelectContent className="bg-background text-textPrimary">
-                  <SelectGroup>
-                    <SelectLabel>Language</SelectLabel>
-                    {codeArray[activeCode].content.map((item, i) => (
-                      <SelectItem key={i} value={`${i}`}>
-                        {item.language}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-
-              {btnClick ? (
-                <button className="rounded   text-green-700 dark:text-green-400 px-2">
-                  <Icons.tick className="w-4 h-4" />
-                </button>
-              ) : (
-                <button
-                  className="rounded  text-textPrimary text-sm px-2"
-                  onClick={handleCopy}
-                >
-                  <Icons.copy className="w-4 h-4" />
-                </button>
-              )}
-            </div>
+                {file.name}
+              </button>
+            ))}
           </div>
-          {/* CONTENT */}
-          <div className="w-full">
-            <CodeHighlight
-              code={StringCleaner(
-                codeArray[activeCode].content[activeLanguage].code
-              )}
-              lang={codeArray[activeCode].content[activeLanguage].language}
-              withCounter={withCounter}
+          <div className="md:hidden flex pb-2 mx-2">
+            <SelectComponent
+              onChange={(val) =>
+                setActiveFile(
+                  files.find((item) => item.name === val) as CodeIndividual
+                )
+              }
+              value={activeFile.name}
+              label="File"
+              items={files.map((item) => item.name)}
+              tooltipMessage="Select the file"
             />
           </div>
+          {/* OPERATOR */}
+          <div className="px-2 sm:px-4 flex pb-2 gap-2">
+            <SelectComponent
+              onChange={(val) =>
+                setActiveColorTheme(val as ColorPaletteSettings)
+              }
+              value={activeColorTheme}
+              label="Color Theme"
+              items={["custom", "tailwind"]}
+              tooltipMessage="Select the color theme: custom means the custom colors that can be used to create your color palette, whereas tailwind means the default Tailwind colors."
+            />
+            <SelectComponent
+              onChange={(val) =>
+                setActiveProvider(val as CodeIndividual["provider"])
+              }
+              value={activeProvider}
+              label="Provider"
+              items={availableData.providers}
+              tooltipMessage="Select the provider: default means the default JSX and TSX, whereas react_aria means the React Aria components."
+            />
+            <SelectComponent
+              onChange={(val) => setActiveType(val as CodeIndividual["type"])}
+              value={activeType}
+              label="Language"
+              items={availableData.types}
+              tooltipMessage="Select the language: js means JavaScript, whereas ts means TypeScript."
+            />
+            <CopyButton copyText="Hello" />
+          </div>
         </div>
-      ) : null}
-    </>
+        {/* CONTENT */}
+        <div className="w-full">
+          <CodeHighlight
+            // code={StringCleaner(activeFile.content)}
+            code={StringCleaner(activeFile.content, activeColorTheme)}
+            lang={activeFile.language}
+            withCounter={withCounter}
+          />
+        </div>
+      </div>
+    </div>
   );
-};
+}
+
+function SelectComponent({
+  items,
+  label,
+  header,
+  onChange,
+  value,
+  tooltipMessage,
+}: {
+  items: string[];
+  label: string;
+  header?: string;
+  onChange: (value: string) => void;
+  value: string;
+  tooltipMessage: string;
+}) {
+  return (
+    <TooltipTrigger delay={0}>
+      <Select
+        aria-label={label}
+        selectedKey={value}
+        onSelectionChange={(val: Key) => onChange(val.toString())}
+        className="w-max text-sm p-0 px-0 py-0 focus-within:outline-none green-500"
+      >
+        <Section>
+          <SelectHeader>{label}</SelectHeader>
+          {header && <SelectHeader>{header}</SelectHeader>}
+          {items.map((item, i) => (
+            <SelectItem id={item} key={i} textValue={item}>
+              {item.replaceAll("_", " ")}
+            </SelectItem>
+          ))}
+        </Section>
+      </Select>
+      {tooltipMessage && (
+        <Tooltip className="max-w-60">{tooltipMessage}</Tooltip>
+      )}
+    </TooltipTrigger>
+  );
+}
+
+function CopyButton({ copyText }: { copyText: string }) {
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  const handleCopy = () => {
+    if (!copyText) return;
+    navigator.clipboard.writeText(copyText);
+    setClicked(true);
+    setTimeout(() => setClicked(false), 2000);
+  };
+
+  return (
+    <TooltipTrigger delay={0} isOpen={clicked ? true : undefined}>
+      <Button
+        className={twMerge(
+          "rounded text-sm w-7 ml-3",
+          clicked ? "text-green-700 dark:text-green-400" : "text-foreground"
+        )}
+        onPress={!clicked ? handleCopy : undefined}
+      >
+        {clicked ? (
+          <Icons.tick className="w-4 h-4" />
+        ) : (
+          <Icons.copy className="w-5 h-5" />
+        )}
+      </Button>
+      <Tooltip> {clicked ? "Copied!" : "Copy"}</Tooltip>
+    </TooltipTrigger>
+  );
+}
---
+++ apps/web/src/utils/StringCleaner.tsx
@@ -0,0 +1,234 @@
+import { useEffect } from "react";
+
+export type ColorPaletteSettings = "custom" | "tailwind";
+
+// Just a few test cases to make sure the regex works as expected
+const TEST_CASES = [
+  "bg-primary",
+  "bg-secondary",
+  "text-outline",
+  "focus:ring-accent",
+  "hover:bg-accent-secondary",
+  "group-focus:ring-accent",
+  "group-hover:bg-accent-foreground",
+  "group-data-[selected=true]:bg-error-secondary",
+  "group-focus:ring-error",
+];
+
+export default function StringCleaner(
+  str: string,
+  colorPalette: ColorPaletteSettings
+): string {
+  if (!str) return "";
+  let output = convertContainerQueries(str);
+  console.log("color", colorPalette);
+
+  if (colorPalette === "tailwind") {
+    // output = replaceColorVariables(output);
+    output = replaceColorVariables(output);
+    console.log("output", output);
+  }
+  return output;
+}
+
+function convertContainerQueries(input: string): string {
+  const containerQueries = [
+    "sm",
+    "md",
+    "lg",
+    "xl",
+    "2xl",
+    "3xl",
+    "4xl",
+    "5xl",
+    "6xl",
+    "7xl",
+  ];
+  let output: string = input;
+
+  for (const query of containerQueries) {
+    const regex = new RegExp(`@${query}:`, "g");
+    if (!output) return "";
+    if (!regex.test(output)) return output;
+    output = output.replace(regex, `${query}:`);
+  }
+
+  return output;
+}
+
+interface ColorPalette {
+  background: string;
+  foreground: string;
+  primary: string;
+  "primary-foreground": string;
+  secondary: string;
+  "secondary-foreground": string;
+  accent: string;
+  "accent-foreground": string;
+  "accent-secondary": string;
+  "accent-secondary-foreground": string;
+  outline: string;
+  "outline-secondary": string;
+  "outline-foreground": string;
+  success: string;
+  "success-secondary": string;
+  "success-foreground": string;
+  alert: string;
+  "alert-secondary": string;
+  "alert-foreground": string;
+  error: string;
+  "error-secondary": string;
+  "error-foreground": string;
+  muted: string;
+  "muted-secondary": string;
+  "muted-foreground": string;
+  blur: string;
+  "blur-secondary": string;
+  "blur-foreground": string;
+}
+
+const lightComponent: ColorPalette = {
+  background: "white",
+  foreground: "black",
+  primary: "neutral-100",
+  "primary-foreground": "neutral-800",
+  secondary: "neutral-200",
+  "secondary-foreground": "neutral-700",
+  accent: "blue-600",
+  "accent-foreground": "white",
+  "accent-secondary": "blue-500",
+  "accent-secondary-foreground": "white",
+  outline: "neutral-500",
+  "outline-secondary": "neutral-300",
+  "outline-foreground": "neutral-500",
+  success: "green-500",
+  "success-secondary": "green-400",
+  "success-foreground": "white",
+  alert: "yellow-500",
+  "alert-secondary": "yellow-400",
+  "alert-foreground": "yellow-900",
+  error: "red-600",
+  "error-secondary": "red-500",
+  "error-foreground": "white",
+  muted: "neutral-600",
+  "muted-secondary": "neutral-200",
+  "muted-foreground": "neutral-200",
+  blur: "white/30",
+  "blur-secondary": "neutral-200/30",
+  "blur-foreground": "neutral-800",
+};
+
+const darkComponent: ColorPalette = {
+  background: "black",
+  foreground: "white",
+  primary: "neutral-900",
+  "primary-foreground": "neutral-100",
+  secondary: "neutral-800",
+  "secondary-foreground": "neutral-400",
+  accent: "blue-600",
+  "accent-foreground": "white",
+  "accent-secondary": "blue-500",
+  "accent-secondary-foreground": "white",
+  outline: "neutral-700",
+  "outline-secondary": "neutral-800",
+  "outline-foreground": "neutral-600",
+  success: "green-800",
+  "success-secondary": "green-600",
+  "success-foreground": "white",
+  alert: "yellow-600",
+  "alert-secondary": "yellow-400",
+  "alert-foreground": "yellow-900",
+  error: "red-600",
+  "error-secondary": "red-500",
+  "error-foreground": "white",
+  muted: "neutral-300",
+  "muted-secondary": "neutral-800",
+  "muted-foreground": "neutral-700",
+  blur: "black/30",
+  "blur-secondary": "neutral-800/30",
+  "blur-foreground": "neutral-200",
+};
+
+const COLOR_TAGS = [
+  "to",
+  "from",
+  "via",
+  "bg",
+  "text",
+  "border",
+  "ring",
+  "placeholder",
+  "fill",
+  "caret",
+  "divide",
+  "stroke",
+  "accent",
+  "shadow",
+  "outline",
+  "decoration",
+  "placeholder",
+  "ring-offset",
+  "border-x",
+  "border-y",
+  "border-t",
+  "border-r",
+  "border-b",
+  "border-l",
+  "border-s",
+  "border-e",
+];
+
+function replaceColorVariables(input: string): string {
+  const colorVariables = Object.keys(lightComponent);
+  let output = input;
+  if (typeof input !== "string" || !output) return "";
+
+  //   increase the spacing between the ""
+  output = output.replaceAll('"', ' " ');
+
+  for (const tag of COLOR_TAGS) {
+    for (const color of colorVariables) {
+      // Regex for normal cases (no prefix)
+      const normalRegex = new RegExp(
+        `(?<![:\\w-])${tag}-${color}(?![\\w-])`,
+        "g"
+      );
+
+      // Regex for prefixed cases, including complex prefixes like attribute selectors
+      const prefixedRegex = new RegExp(
+        `(^|\\s)([^\\s]+:)(\\S*${tag}-${color})(?=\\s|$)`,
+        "g"
+      );
+
+      const normalPass = normalRegex.test(output);
+      const prefixedPass = prefixedRegex.test(output);
+
+      if (normalPass) {
+        // Handle normal cases
+        output = output.replace(normalRegex, (match) => {
+          const lightColor = (lightComponent as any)[color];
+          const darkColor = (darkComponent as any)[color];
+
+          return `${tag}-${lightColor} dark:${tag}-${darkColor}`;
+        });
+      }
+      if (prefixedPass) {
+        // Handle prefixed cases
+        output = output.replace(
+          prefixedRegex,
+          (match, space, prefix, className) => {
+            const lightColor = (lightComponent as any)[color];
+            const darkColor = (darkComponent as any)[color];
+
+            // Ensure the prefix is correctly applied to both light and dark colors
+            return `${space}${prefix}${tag}-${lightColor} ${space}dark:${prefix}${tag}-${darkColor}`;
+          }
+        );
+      }
+    }
+  }
+
+  //   Change the spacing between the "" to a single space
+  output = output.replaceAll(' " ', '"');
+  return output;
+}
