1213979d0ac5f16733b3f52bd0df5d1155396025
krishpatel023 <krishpatel0234@gmail.com>
2024-08-14 18:44:17 +0530

    web/add: added few internal states (#53)
    These states like provider, language, etc will help in improving the user experience and were necessary for the sync between components like details, etc

--- apps/web/src/hooks/useInternalState.tsx
+++ apps/web/src/hooks/useInternalState.tsx
@@ -7,11 +7,23 @@ const InternalStateContext = createContext<{
   setSidebar: (open: boolean) => void;
   searchbar: boolean;
   setSearchbar: (open: boolean) => void;
+  provider: "default" | "react_aria";
+  setProvider: (provider: "default" | "react_aria") => void;
+  language: "js" | "ts";
+  setLanguage: (language: "js" | "ts") => void;
+  themingOption: "tailwind" | "custom";
+  setThemingOption: (themingOption: "tailwind" | "custom") => void;
 }>({
   sidebar: false,
   setSidebar: () => {},
   searchbar: false,
   setSearchbar: () => {},
+  provider: "react_aria",
+  setProvider: () => {},
+  language: "ts",
+  setLanguage: () => {},
+  themingOption: "tailwind",
+  setThemingOption: () => {},
 });
 
 export const useInternalState = () => useContext(InternalStateContext);
@@ -24,9 +36,29 @@ export const InternalStateProvider = ({
   const [sidebar, setSidebar] = useState<boolean>(false);
   const [searchbar, setSearchbar] = useState<boolean>(false);
 
+  const [provider, setProvider] = useState<"default" | "react_aria">(
+    "react_aria"
+  );
+  const [language, setLanguage] = useState<"js" | "ts">("ts");
+
+  const [themingOption, setThemingOption] = useState<"tailwind" | "custom">(
+    "tailwind"
+  );
+
   return (
     <InternalStateContext.Provider
-      value={{ sidebar, setSidebar, searchbar, setSearchbar }}
+      value={{
+        sidebar,
+        setSidebar,
+        searchbar,
+        setSearchbar,
+        provider,
+        setProvider,
+        language,
+        setLanguage,
+        themingOption,
+        setThemingOption,
+      }}
     >
       {children}
     </InternalStateContext.Provider>
