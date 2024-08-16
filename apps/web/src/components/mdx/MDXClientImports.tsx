50f5265b731ccf7544f701400ea37d068c4abfce
krishpatel023 <krishpatel0234@gmail.com>
2024-08-16 10:18:39 +0530

    web/add: loading states

--- apps/web/src/components/mdx/MDXClientImports.tsx
+++ apps/web/src/components/mdx/MDXClientImports.tsx
@@ -1,9 +1,13 @@
 "use client";
 import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/Skeleton";
 
 const CodeBlock = dynamic(
   () => import("@/components/mdx/Code").then((mod) => mod.CodeBlock),
-  { ssr: false }
+  {
+    loading: () => <Skeleton className="min-h-60 min-w-full rounded-lg" />,
+    ssr: false,
+  }
 );
 
 import ResizableDisplay from "@/components/mdx/ResizableDisplay";
