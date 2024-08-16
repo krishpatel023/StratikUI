d0bc56fd76d0b38f445e73219b05809b2515e697
krishpatel023 <krishpatel0234@gmail.com>
2024-08-16 09:26:38 +0530

    web/fix: documentation layout
    The docs layout for the small screen version was not optimised. It was applying the spacing and formating of the large screen

--- apps/web/src/app/docs/(manualDocs)/layout.tsx
+++ apps/web/src/app/docs/(manualDocs)/layout.tsx
@@ -5,7 +5,9 @@ export default function Layout({
 }>) {
   return (
     <>
-      <div className="w-[85%] mx-auto py-16 mb-4">{children}</div>
+      <div className="w-full px-4 md:px-0 md:w-[85%] mx-auto py-12 md:py-16 mb-4">
+        {children}
+      </div>
     </>
   );
 }
