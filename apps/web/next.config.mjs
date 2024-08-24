c72ec29d26f42125a7075398e20081608c6eb0ee
krishpatel023 <krishpatel0234@gmail.com>
2024-08-24 11:32:05 +0530

    delete: removed eslint and replaced it with biome (#68)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -14,6 +14,10 @@ const withMDX = nextMDX({
 
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  eslint: {
+    // Warning: This is implemented as we want to use biome to lint the code. We have added the linting before the build step.
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default withMDX(nextConfig);
